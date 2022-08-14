08c1d75779167a6536b37b7e45906bf2b2cf2e15
Jikky1618 <Jikky0110@gmail.com>
2022-08-14 14:26:23 +0900

    commit

---
+++ ABS/8_ABC085b.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+    int n,res = 0;
+    cin >> n;
+    vector<int> d(n);
+    for(int i = 0; i < n; i++) cin >> d[i];
+    vector<int > num(101);
+    for(int i = 0; i < n; i++){
+        num[d[i]]++;
+    }
+    for(int i = 0; i < 101; i++){
+        if(num[i] > 0){
+            res++;
+        }
+    }
+    cout << res << endl;
+    
+    return 0;
+}
