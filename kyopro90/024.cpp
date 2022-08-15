375e41fb55e491f4d760ad7009923df1666d233a
Jikky1618 <Jikky0110@gmail.com>
2022-08-15 22:57:27 +0900

    Update 024.cpp

--- kyopro90/024.cpp
+++ kyopro90/024.cpp
@@ -2,14 +2,15 @@
 using namespace std;
 
 int main(void){
-    // input
+
+    // Input
     int n,k;
     cin >> n >> k;
     vector<int> a(n),b(n);
     for(int i = 0; i < n; i++) cin >> a[i];
     for(int i = 0; i < n; i++) cin >> b[i];
 
-    // check diff
+    // Check diff
     long long int diff = 0;
     for(int i = 0; i < n; i++){
         diff += abs(a[i] - b[i]);
@@ -19,12 +20,13 @@ int main(void){
         return 0;
     }
 
-    // check parity
+    // Check parity
     if(k % 2 != diff % 2){
         cout << "No" << endl;
         return 0;
     }
-
+    
+    // Output
     cout << "Yes" << endl;
 
     return 0;
