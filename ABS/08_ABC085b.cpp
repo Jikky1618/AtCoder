3ae541b40ab880733aaccbdf730b2d939174831f
Jikky1618 <Jikky0110@gmail.com>
2022-08-15 13:56:42 +0900

    commit

--- ABS/08_ABC085b.cpp
+++ ABS/08_ABC085b.cpp
@@ -6,7 +6,7 @@ int main(void){
     cin >> n;
     vector<int> d(n);
     for(int i = 0; i < n; i++) cin >> d[i];
-    vector<int > num(101);
+    vector<int> num(101);
     for(int i = 0; i < n; i++){
         num[d[i]]++;
     }
