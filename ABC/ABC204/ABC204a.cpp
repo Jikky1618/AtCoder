5a61d8c1a671e7b2187cc19d6ef6de18ae02c12f
Jikky1618 <Jikky0110@gmail.com>
2022-08-15 16:56:50 +0900

    commit

---
+++ ABC/ABC204/ABC204a.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+    int x,y;
+    cin >> x >> y;
+    if(x == y) cout << x << endl;
+    else cout << 3 - x - y << endl;
+    
+    return 0;
+}
