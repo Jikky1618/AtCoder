6da5001c1b16bcdc04dece805a0ff9ef05ba81e0
Jikky1618 <Jikky0110@gmail.com>
2022-08-15 18:38:29 +0900

    commit

---
+++ ABC/ABC199/ABC199a.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+    int a,b,c;
+    cin >> a >> b >> c;
+    if(a * a + b * b < c * c) cout << "Yes" << endl;
+    else cout << "No" << endl;
+
+    return 0;
+}
