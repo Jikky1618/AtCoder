c924779a0b64a7a8cf25e52c152203e335707efb
Jikky1618 <Jikky0110@gmail.com>
2022-08-21 13:14:49 +0900

    commit

--- ABC/ABC208/b.cpp
+++ ABC/ABC208/b.cpp
@@ -4,6 +4,22 @@ using namespace std;
 int main(){
     int p;
     cin >> p;
+    vector<int> factorial(11);
+    factorial[0] = 1;
+    // 1-index
+    for(int i = 1; i <= 10; i++){
+        factorial[i] = i * factorial[i - 1];
+    }
+    
+    int ans = 0;
+    for(int i = 10; i >= 1; i--){
+        while(factorial[i] <= p){
+            p -= factorial[i];
+            ans++;
+        }
+    }
+
+    cout << ans << endl;
     
     return 0;
 }
