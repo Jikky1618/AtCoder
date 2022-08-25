46f7381f357b3e1896e7378af1e4890d5639a86f
Jikky1618 <Jikky0110@gmail.com>
2022-08-25 09:24:28 +0900

    Commit

--- ABC/ABC205/c.cpp
+++ ABC/ABC205/c.cpp
@@ -1,16 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
 
-int main(void){
+int main(){
+    // Input
     int a,b,c;
     cin >> a >> b >> c;
+
+    // pow(a,c),pow(b,c)の符号check
     int _ac = c % 2 == 1 && a < 0;
     int _bc = c % 2 == 1 && b < 0;
-    if(_ac != _bc){
-        cout << (_ac > _bc ? "<" : ">") << endl;
-    }else{
+
+    //Check
+    if(_ac == _bc){
+        if(abs(a) > abs(b)) cout << ">" << endl;
+        if(abs(a) < abs(b)) cout << "<" << endl;
         if(abs(a) == abs(b)) cout << "=" << endl;
-        else cout << (abs(a) > abs(b) ? ">" : "<") << endl;
+    }else{
+        if(a < 0) cout << "<" << endl;
+        if(b < 0) cout << ">" << endl;
     }
+    
     return 0;
 }
