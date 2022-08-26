40161cf320b4e756b17da17f254fc0b825ee1313
Jikky1618 <Jikky0110@gmail.com>
2022-08-27 00:31:53 +0900

    Commit

---
+++ ABC/ABC213/b_pair.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(){
+    int n;
+    cin >> n;
+    vector<pair<int,int>> a(n);
+    for(int i = 0; i < n; i++){
+        cin >> a[i].first;
+        a[i].second = i + 1;
+    };
+
+    sort(a.begin(), a.end(),greater<pair<int,int>>());
+
+    cout << a[1].second <<endl;
+
+    return 0;
+}
