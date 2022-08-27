935b01d94b195679b9fa8bfcf95cd14f1ceb6c8e
Jikky1618 <Jikky0110@gmail.com>
2022-08-28 00:55:49 +0900

    Commit

---
+++ ABC/ABC215/a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(){
+    string str;
+    cin >> str;
+    if(str == "Hello,World!") cout << "AC" << endl;
+    else cout << "WA" << endl;
+
+    return 0;
+}
