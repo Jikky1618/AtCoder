7ba3b7324cf5c5b65b6d76a9af52129a33ca3d3b
Jikky1618 <Jikky0110@gmail.com>
2022-08-29 08:53:38 +0900

    Commit

---
+++ ABC/ABC218/a.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(){
+    int n;
+    string str;
+    cin >> n >> str;
+
+    if(str[n-1] =='o') cout << "Yes" << endl;
+    else cout << "No" << endl;
+    
+    return 0;
+}
