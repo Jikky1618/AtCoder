17786690cd52c029fba3f0b49408a91f2d188cd6
Jikky1618 <Jikky0110@gmail.com>
2022-08-29 09:00:36 +0900

    Commit

---
+++ test.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(){
+    int n,x_1;
+    cin >> n >> x_1;
+    vector<int> d(n);
+    for(int i = 0; i < n; i++) cin >> d[i];
+
+    for(int i = 0; i < n; i++){
+        
+    }
+
+    return 0;
+}
