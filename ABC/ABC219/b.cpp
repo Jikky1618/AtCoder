cd46c7c76373fb2ee13061bca334a1da504ff177
Jikky1618 <Jikky0110@gmail.com>
2022-08-30 23:46:43 +0900

    Solved ABC219_B

--- ABC/ABC219/b.cpp
+++ ABC/ABC219/b.cpp
@@ -7,9 +7,11 @@ int main(){
     string t;
     cin >> s[0] >> s[1] >> s[2] >> t;
 
-    string ans;
-    for(int i = 0; i < t.length(); i++){ // thonk
-        ans += s[t[i]];
+    string ans = "";
+    for(int i = 0; i < t.size(); i++){
+        // s[t[i](char) - '1'(char)]
+        // 1 is not type int
+        ans += s[t[i] - '1'];
     }
 
     cout << ans << endl;
