33bedf0ae639526571eb4687cdf4ad35b48028eb
Jikky1618 <Jikky0110@gmail.com>
2022-08-31 03:35:54 +0900

    Commit

---
+++ ABC/ABC224/a.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(){
+    string str;
+    cin >> str;
+
+    if(str[str.size()-1] == 'r') cout << "er" << endl;
+    else cout << "ist" << endl;
+
+    return 0;
+}
