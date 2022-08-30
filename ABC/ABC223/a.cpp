627fd929ee288f357568f52812ee3fb5e28ed0c7
Jikky1618 <Jikky0110@gmail.com>
2022-08-30 22:46:10 +0900

    Commit

---
+++ ABC/ABC223/a.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(){
+    int x;
+    cin >> x;
+
+    if(x % 100 == 0 && x > 0) cout << "Yes" << endl;
+    else cout << "No" << endl;
+
+    return 0;
+}
