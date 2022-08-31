7fe25db9f1d6b1b5e3509d551a5681942aaa0c69
Jikky1618 <Jikky0110@gmail.com>
2022-08-31 22:09:01 +0900

    Commit

---
+++ ABC/ABC230/a.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(){
+    int n;
+    scanf("%d",&n);
+
+    if(n >= 42) n++;
+
+    printf("AGC%03d",n);
+
+    return 0;
+}
