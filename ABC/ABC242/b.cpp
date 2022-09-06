1ba3c7f4cc2ea4d81ecec02ff7914a6bd6a93bc0
Jikky1618 <Jikky0110@gmail.com>
2022-09-07 06:37:21 +0900

    Commit

---
+++ ABC/ABC242/b.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(){
+    string s;
+    cin >> s;
+
+    sort(s.begin(), s.end());
+
+    cout << s << endl;
+
+    return 0;
+}
