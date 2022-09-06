14431d11f4a131ddb2b697c5fc282e61081439e4
Jikky1618 <Jikky0110@gmail.com>
2022-09-07 02:45:00 +0900

    Commit

--- ABC/ABC226/b.cpp
+++ ABC/ABC226/b.cpp
@@ -5,7 +5,16 @@ using ll = long long;
 int main(){
     int n;
     cin >> n;
-    
+    set<vector<int>> seq;
+    for(int i = 0; i < n; i++){
+        int l;
+        cin >> l;
+        vector<int> v(l);
+        for(int j = 0; j < l; j++) cin >> v[j];
+        seq.insert(v);
+    }
 
+    cout << seq.size() << endl;
+    
     return 0;
 }
