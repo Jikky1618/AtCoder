af6df805850209274c1190caa02dcc876859e04d
Jikky1618 <Jikky0110@gmail.com>
2022-09-08 06:22:10 +0900

    Commit

---
+++ ABC/ABC251/a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(){
+    string s;
+    cin >> s;
+    
+    for(int i = 0; i < 6/s.length(); i++){
+        cout << s;
+    }
+    cout << endl;
+
+    return 0;
+}
