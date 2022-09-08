0dc9e85feb89dfe6528010940f14d4edd30d7e27
Jikky1618 <Jikky0110@gmail.com>
2022-09-09 07:39:34 +0900

    Commit

--- ABC/ABC263/a.cpp
+++ ABC/ABC263/a.cpp
@@ -7,10 +7,11 @@ int main(){
     for(int i = 0; i < 5; i++){
         int num;
         cin >> num;
-        in[num]++;
+        in[num]++; // 各数字の出現回数
     }
     
     for(int i = 0; i < 13; i++){
+        // フルハウスのとき、出現するカードの枚数は0,2,3枚
         if(in[i] == 1 || in[i] == 4 || in[i] == 5){
             cout << "No" << endl;
             return 0;
