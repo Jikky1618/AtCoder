dbb56f96d13902ad0985989664d3e3be47f84a62
Jikky1618 <Jikky0110@gmail.com>
2022-09-09 10:18:28 +0900

    Commit

---
+++ ABC/ABC266/a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(){
+    string s;
+    cin >> s;
+
+    cout << s[(s.length() + 1) / 2 - 1] << endl;
+
+    return 0;
+}
