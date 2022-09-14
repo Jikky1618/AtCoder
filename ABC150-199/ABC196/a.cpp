b7af51f9b3534226185b6c2e4ce4836ca71293bb
Jikky1618 <Jikky0110@gmail.com>
2022-09-14 23:10:19 +0900

    Commit

---
+++ ABC150-199/ABC196/a.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    int a,b,c,d;
+    cin >> a >> b >> c >> d;
+
+    cout << b - c << endl;
+
+    return 0;
+}
