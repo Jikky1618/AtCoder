e75514e4761eb0786fbcbf2a4dcbd73c99fab34f
Jikky1618 <Jikky0110@gmail.com>
2022-09-22 02:12:13 +0900

    Commit

---
+++ ABC/ABC250-299/ABC269/c.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    ull n;
+    cin >> n;
+    vector<ull> ans;
+
+    return 0;
+}
