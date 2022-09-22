77cd4f1872ebb28d541bfe7b83cc87913b244c4e
Jikky1618 <Jikky0110@gmail.com>
2022-09-22 12:42:54 +0900

    Commit

--- ABC/ABC250-299/ABC269/c.cpp
+++ ABC/ABC250-299/ABC269/c.cpp
@@ -1,14 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-using ull = unsigned long long;
 
 int main(){
     cin.tie(nullptr);
     ios::sync_with_stdio(false);
-    ull n;
+    ll n;
     cin >> n;
-    vector<ull> ans;
+
+    vector<int> one;
+    // 60桁全探索
+    for(int i = 0; i < 60; i++){
+        // i桁目が1ならiを要素に追加
+        if((n >> i) & 1) one.push_back(i);
+    }
+
+    vector<ll> ans;
+    // bit全探索
+    for(int bit = 0; bit < (1 << (int)one.size()); bit++){
+        ll now = 0;
+        for(int j = 0; j < (int)one.size(); j++){
+            // bitのj桁目が1なら1をone[j]桁目にshift
+            if((bit >> j) & 1) now += 1LL << one[j];
+        }
+        ans.push_back(now);
+    }
+
+    for(int i = 0; i < (int)ans.size(); i++) cout << ans[i] << endl;
 
     return 0;
 }
