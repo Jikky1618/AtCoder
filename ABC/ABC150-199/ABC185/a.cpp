4db871d20bb30e986cf9e006fa18534271c64754
Jikky1618 <Jikky0110@gmail.com>
2022-09-24 17:07:18 +0900

    Commit

---
+++ ABC/ABC150-199/ABC185/a.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    int a1,a2,a3,a4;
+    cin >> a1 >> a2 >> a3 >> a4;
+    cout << min({a1,a2,a3,a4}) << endl;
+    
+    return 0;
+}
