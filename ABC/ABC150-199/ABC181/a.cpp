19583b57b9401f63be948c52a88e3dae3fb50060
Jikky1618 <Jikky0110@gmail.com>
2022-09-24 17:13:51 +0900

    Commit

---
+++ ABC/ABC150-199/ABC181/a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    int n;
+    cin >> n;
+
+    if(n % 2 == 0) cout << "White" << endl;
+    else cout << "Black" << endl;
+    
+    return 0;
+}
