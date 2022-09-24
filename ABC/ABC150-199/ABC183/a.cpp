2a6f70671f57c8c012a5935506387bbb11f0535c
Jikky1618 <Jikky0110@gmail.com>
2022-09-24 17:10:41 +0900

    Commit

---
+++ ABC/ABC150-199/ABC183/a.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    int x;
+    cin >> x;
+    cout << max(x,0) << endl;
+    
+    return 0;
+}
