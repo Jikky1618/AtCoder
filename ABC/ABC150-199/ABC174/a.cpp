04528297cd5037fe0ad36a0019a38f7f9e95ee63
Jikky1618 <Jikky0110@gmail.com>
2022-09-25 17:14:24 +0900

    Commit

---
+++ ABC/ABC150-199/ABC174/a.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    int x;
+    cin >> x;
+    cout << (x >= 30 ? "Yes" : "No") << endl;
+
+    return 0;
+}
