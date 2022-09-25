679181617daef6602b8a048ffa6723d1e49818d8
Jikky1618 <Jikky0110@gmail.com>
2022-09-25 21:49:10 +0900

    Commit

---
+++ ABC/ABC150-199/ABC168/b.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    int k;
+    string s;
+    cin >> k >> s;
+    if(s.size() <= k) cout << s << endl;
+    else cout << s.substr(0,k) << "..." <<  endl;
+    return 0;
+}
