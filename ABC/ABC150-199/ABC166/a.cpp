d319aa4f4fa351f310c2a74f2314a8dad6b467d9
Jikky1618 <Jikky0110@gmail.com>
2022-09-25 22:06:34 +0900

    Commit

---
+++ ABC/ABC150-199/ABC166/a.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    string s;
+    cin >> s;
+    if(s == "ABC") cout << "ARC" << endl;
+    else if(s == "ARC") cout << "ABC" << endl;
+    return 0;
+}
