d7eab064d2464ebb1174a71f9f845eca691a3a2a
Jikky1618 <Jikky0110@gmail.com>
2022-09-27 04:40:44 +0900

    Commit

--- ABC/ABC150-199/ABC158/b.cpp
+++ ABC/ABC150-199/ABC158/b.cpp
@@ -5,6 +5,14 @@ using ll = long long;
 int main(){
     cin.tie(nullptr);
     ios::sync_with_stdio(false);
-    int 
+    ll n,a,b;
+    cin >> n >> a >> b;
+
+    ll ans = 0;
+    ans += n / (a + b) * a;
+    ans += min(a,n % (a + b));
+
+    cout << ans << endl;
+    
     return 0;
 }
