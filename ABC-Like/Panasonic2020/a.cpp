a72889fb7a6a98d0f590852da15439e274e977ad
Jikky1618 <Jikky0110@gmail.com>
2022-09-27 03:40:46 +0900

    Commit

---
+++ ABC-Like/Panasonic2020/a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    vector<int> seq = {1, 1, 1, 2, 1, 2, 1, 5, 2, 2, 1, 5, 1, 2, 1, 14, 1, 5, 1, 5, 2, 2, 1, 15, 2, 2, 5, 4, 1, 4, 1, 51};
+    int k;
+    cin >> k;
+
+    cout << seq[k - 1] << endl;
+    
+    return 0;
+}
