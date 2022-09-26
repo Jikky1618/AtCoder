afa4cc17a6212a1113b8811829a777d53d1e8738
Jikky1618 <Jikky0110@gmail.com>
2022-09-27 08:35:18 +0900

    Add kyopro-tessoku

---
+++ kyopro-tessoku/A01.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    int n;
+    cin >> n;
+    cout << (n * n) << endl;
+    return 0;
+}
