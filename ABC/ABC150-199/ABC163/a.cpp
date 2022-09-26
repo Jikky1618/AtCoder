7b4967cbd8a4afdd34139ae59645a46817375476
Jikky1618 <Jikky0110@gmail.com>
2022-09-27 01:36:43 +0900

    Commit

---
+++ ABC/ABC150-199/ABC163/a.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+const double PI = acos(-1);
+
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    int r;
+    cin >> r;
+    
+    double ans = r * 2 * PI;
+    
+    cout << ans << endl;
+    return 0;
+}
