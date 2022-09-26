dda897b68fedecd65e5dd624a92af380513cee0c
Jikky1618 <Jikky0110@gmail.com>
2022-09-27 01:15:27 +0900

    Commit

---
+++ ABC/ABC150-199/ABC164/a.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    int s,w;
+    cin >> s >> w;
+    cout << (w >= s ? "unsafe" : "safe") << endl;
+    
+    return 0;
+}
