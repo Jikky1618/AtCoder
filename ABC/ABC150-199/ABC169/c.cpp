ecec580e7f06a29aed3f912dc4ce53b2c7392e1f
Jikky1618 <Jikky0110@gmail.com>
2022-09-26 23:00:32 +0900

    Update ABC169/c.cpp

--- ABC/ABC150-199/ABC169/c.cpp
+++ ABC/ABC150-199/ABC169/c.cpp
@@ -2,14 +2,20 @@
 using namespace std;
 using ll = long long;
 
-int main(){
-    ll a,b1,b2;
-    scanf("%lld %lld.%lld",&a,&b1,&b2);
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    ll a;
+    double b;
+    cin >> a >> b;
 
-    ll ans = a * (b1 * 100 + b2);
-    ans /= 100;
+    // bを整数として扱うために100倍
+    // 浮動小数点数型を整数型にキャストする際は切り捨て
+    // 正しく丸めるためにキャストする際にllroundで四捨五入
+    ll fix_b = llround(b * 100);
+    // fix_bの修正分として100で割る
+    ll ans = a * fix_b / 100;
 
-    printf("%lld",ans);
-    
+    cout << ans << endl;
     return 0;
 }
