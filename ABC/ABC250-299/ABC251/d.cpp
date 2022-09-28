30f19ccd6324d51dca7edd317226468251312795
Jikky1618 <Jikky0110@gmail.com>
2022-09-29 06:17:28 +0900

    Commit

---
+++ ABC/ABC250-299/ABC251/d.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    int w;
+    cin >> w;
+
+    cout << 297 << endl;
+    for(int i = 1; i <= 99; i++) cout << i << endl;
+    for(int i = 1; i <= 99; i++) cout << i * 100 << endl;
+    for(int i = 1; i <= 99; i++) cout << i * 10000 << endl;
+    return 0;
+}
