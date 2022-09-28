e330a071d27d55e35dd667f2b0268f09de644e70
Jikky1618 <Jikky0110@gmail.com>
2022-09-29 01:07:52 +0900

    Commit

---
+++ ABC/ABC100-149/ABC143/a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    int a,b;
+    cin >> a >> b;
+    cout << max(0,a - b * 2) << endl;
+    return 0;
+}
