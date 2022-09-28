0858fc04ef765ab404776e777538fe7240a108ac
Jikky1618 <Jikky0110@gmail.com>
2022-09-29 05:49:16 +0900

    Commit

--- ABC/ABC250-299/ABC251/b.cpp
+++ ABC/ABC250-299/ABC251/b.cpp
@@ -2,39 +2,28 @@
 using namespace std;
 using ll = long long;
 
-int main()
-{
+int main(){
     int n, w;
     cin >> n >> w;
     vector<int> a(n);
-    for (int i = 0; i < n; i++)
-        cin >> a[i];
+    for (int i = 0; i < n; i++) cin >> a[i];
 
     set<int> st;
-    for (int i = 0; i < n; i++)
-    {
-        if (a[i] <= w)
-            st.insert(a[i]);
+    for (int i = 0; i < n; i++){
+        if (a[i] <= w) st.insert(a[i]);
     }
 
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = i + 1; j < n; j++)
-        {
-            if (a[i] + a[j] <= w)
-            {
+    for (int i = 0; i < n; i++){
+        for (int j = i + 1; j < n; j++){
+            if (a[i] + a[j] <= w){
                 st.insert(a[i] + a[j]);
             }
         }
     }
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = i + 1; j < n; j++)
-        {
-            for (int k = j + 1; k < n; k++)
-            {
-                if (a[i] + a[j] + a[k] <= w)
-                {
+    for (int i = 0; i < n; i++){
+        for (int j = i + 1; j < n; j++){
+            for (int k = j + 1; k < n; k++){
+                if (a[i] + a[j] + a[k] <= w){
                     st.insert(a[i] + a[j] + a[k]);
                 }
             }
