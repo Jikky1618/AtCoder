620bd3a888c1ce2ffee23f17b17b6d0c1465f2a9
Jikky1618 <Jikky0110@gmail.com>
2022-10-01 03:57:26 +0900

    COmmit

--- ABC/ABC100-149/ABC145/c.cpp
+++ ABC/ABC100-149/ABC145/c.cpp
@@ -13,13 +13,15 @@ int main(){
     double sum = 0,cnt = 0;
     do{
         for(int i = 0; i < n - 1; i++){
-            double dist = sqrt(pow(x[p[i]] - x[p[i + 1]],2) + pow(y[p[i]] - y[p[i + 1]],2));
+            long double dx = x[p[i]] - x[p[i + 1]];
+            long double dy = y[p[i]] - y[p[i + 1]];
+            long double dist = sqrt(dx * dx + dy * dy);
             sum += dist;
         }
         cnt++;
     }while(next_permutation(p.begin(), p.end()));
     double ans = sum / cnt;
-    
+
     printf("%.10lf",ans);
     return 0;
 }
