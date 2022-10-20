327ee0e1cc1c118df1fe42c245f578e8350f473b
Jikky1618 <Jikky0110@gmail.com>
2022-10-20 17:55:58 +0900

    Commit

---
+++ ABC/ABC100-149/ABC114/a.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    int n;
+    cin >> n;
+
+    if(n == 7 || n == 5 || n == 3){
+        cout << "YES" << endl;
+    }else{
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
