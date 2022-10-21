1dff346ca12d106384b06396e104820ea3a76a41
Jikky1618 <Jikky0110@gmail.com>
2022-10-21 21:49:08 +0900

    Commit

---
+++ ABC/ABC150-199/ABC159/c.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(10);
+    double l;
+    cin >> l;
+    cout << pow((l / 3), 3) << endl;
+    return 0;
+}
