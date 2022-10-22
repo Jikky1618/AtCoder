0e07b6138cf45d34ab462a5ffca6597a69bf4ed5
Jikky1618 <Jikky0110@gmail.com>
2022-10-23 01:43:31 +0900

    Add ABC274

--- ABC/ABC250-299/ABC274/d.cpp
+++ ABC/ABC250-299/ABC274/d.cpp
@@ -15,28 +15,31 @@ int main(){
     for(int i = 0; i < n; i++) cin >> a[i];
 
     // dpテーブル
-    vector<int> dpx(2 * M + 1), dpy(2 * M + 1);
+    vector<int> dpx(2 * M + 1), dpy(2 * M + 1), dp_tmp(2 * M + 1);
     // 初期位置
     dpx[a[0] + M] = dpy[0 + M] = 1;
 
     for(int i = 1; i < n; i++){
+        for (int j = -M; j <= M; j++) dp_tmp[j + M] = 0; // 初期化
         // 奇数番目の操作(x方向)の場合
         if(i % 2 == 0){
             for(int j = -M; j <= M - a[i]; j++){
-                dpx[j + a[i] + M] |= dpx[j + M];
+                dp_tmp[j + a[i] + M] |= dpx[j + M];
             }
             for(int j = -M + a[i]; j <= M; j++){
-                dpx[j - a[i] + M] |= dpx[j + M];
+                dp_tmp[j - a[i] + M] |= dpx[j + M];
             }
+            swap(dp_tmp, dpx);
         }
         // 偶数番目の操作(y方向)の場合
         if(i % 2 == 1){
             for(int j = -M; j <= M - a[i]; j++){
-                dpy[j + a[i] + M] |= dpy[j + M];
+                dp_tmp[j + a[i] + M] |= dpy[j + M];
             }
             for(int j = -M + a[i]; j <= M; j++){
-                dpy[j - a[i] + M] |= dpy[j + M];
+                dp_tmp[j - a[i] + M] |= dpy[j + M];
             }
+            swap(dp_tmp, dpy);
         }
     }
 
