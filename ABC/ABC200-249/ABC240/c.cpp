d6d8fc1ce8e5e48d4044eb5349902d7e0902b9df
Jikky1618 <Jikky0110@gmail.com>
2022-10-23 15:43:51 +0900

    Commit

---
+++ ABC/ABC200-249/ABC240/c.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    int n,x;
+    cin >> n >> x;
+    vector<int> a(n), b(n);
+    for(int i = 0; i < n; i++) cin >> a[i] >> b[i];
+
+    vector<vector<int>> dp(n + 1,vector<int>(x + 1));
+    dp[0][0] = 1;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < x; j++){
+            if(j + a[i] <= x) dp[i + 1][j + a[i]] |= dp[i][j];
+            if(j + b[i] <= x) dp[i + 1][j + b[i]] |= dp[i][j];
+        }
+    }
+
+    if(dp[n][x]) cout << "Yes" << endl;
+    else cout << "No" << endl;
+    return 0;
+}
