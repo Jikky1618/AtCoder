d01ea8c44584400c2474a1e4234b1d3379c60b97
Jikky1618 <Jikky0110@gmail.com>
2022-10-24 16:29:10 +0900

    Commit

---
+++ ABC/ABC150-199/ABC157/c.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    int n,m;
+    cin >> n >> m;
+    vector<int> s(m);
+    vector<char> c(m);
+    for(int i  = 0; i < m; i++) cin >> s[i] >> c[i];
+
+    for(int i = 0; i < 999; i++){
+        if(to_string(i).size() != n) continue;
+        bool flag = true;
+        for(int j = 0; j < m; j++){
+            if(to_string(i).at(s[j] - 1) != c[j]) flag = false;
+        }
+        if(flag){
+            cout << i << endl;
+            return 0;
+        }
+    }
+
+    cout << -1 << endl;
+    return 0;
+}
