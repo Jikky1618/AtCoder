75ece6ffacee7cc436ced987fe7c09fbcef7f325
Jikky1618 <Jikky0110@gmail.com>
2022-10-29 03:24:49 +0900

    Add A10.cpp

--- kyopro-tessoku/A/A10.cpp
+++ kyopro-tessoku/A/A10.cpp
@@ -17,11 +17,10 @@ int main(){
     // 累積max
     vector<int> p(n+1),q(n+1);
     for(int i = 0; i < n; i++) p[i+1] = max(p[i], a[i]);
-    for(int i = n ; i > 0; i--) q[i-1] = max(q[i], a[i-1]);
+    for(int i = 0; i < n; i++) q[i+1] = max(q[i], a[n-i-1]);
     
     for(int i = 0; i < d; i++){
-        cout << max(p[l[i]-1], q[r[i]]) << endl;
+        cout << max(p[l[i]-1], q[n-r[i]]) << endl;
     }
-
     return 0;
 }
