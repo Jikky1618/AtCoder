a4d6196ec3a40cbcbd4d02de9594bf06af24b1fb
Jikky1618 <Jikky0110@gmail.com>
2022-10-31 15:23:51 +0900

    Add A13.cpp

--- kyopro-tessoku/A/A13.cpp
+++ kyopro-tessoku/A/A13.cpp
@@ -5,16 +5,24 @@ using ll = long long;
 int main(){
     cin.tie(nullptr);
     ios::sync_with_stdio(false);
-    int n,k;
+    ll n,k;
     cin >> n >> k;
-    vector<int> a(n);
+    vector<ll> a(n);
     for(int i = 0; i < n; i++) cin >> a[i];
 
-    sort(a.begin(), a.end());
-    int ans = 0;
+    // しゃくとり法
+    vector<int> r(n);
     for(int i = 0; i < n; i++){
-        ans += upper_bound(a.begin(), a.end(),a[i] - k) - a.begin();
+        if(i == 0) r[i] = 0;
+        else r[i] = r[i-1];
+        // ギリギリまで増やす
+        while(r[i] < n-1 && a[r[i]+1]-a[i] <= k){
+            r[i]++;
+        }
     }
+
+    ll ans = 0;
+    for(int i = 0; i < n; i++) ans += r[i] - i;
 
     cout << ans << endl;
     return 0;
