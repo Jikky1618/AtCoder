d59094d2c04929b1c9fe4d7a8fe3191b7230e159
Jikky1618 <Jikky0110@gmail.com>
2022-10-31 12:25:04 +0900

    Commit

--- ABC/ABC100-149/ABC125/c.cpp
+++ ABC/ABC100-149/ABC125/c.cpp
@@ -12,12 +12,12 @@ int main(){
     for(int i = 0; i < n; i++) cin >> a[i];
 
     vector<int> left(n+1), right(n+1);
-    for(int i = 0; i < n; i++) left[i + 1] =  __gcd(left[i], a[i]);
-    for(int i = 0; i < n; i++) right[i + 1] = __gcd(right[i], a[n-i-1]);
+    for(int i = 0; i < n; i++) left[i + 1] =  gcd(left[i], a[i]);
+    for(int i = 0; i < n; i++) right[i + 1] = gcd(right[i], a[n-i-1]);
 
     int ans = 0;
     for(int i = 0; i < n; i++){
-        ans = max(ans, __gcd(left[i], right[n-i-1]));
+        ans = max(ans, gcd(left[i], right[n-i-1]));
     }
     cout << ans << endl;
     return 0;
