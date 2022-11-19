1d465375bbcaa304238511b897bd28ae136d3dd6
Jikky1618 <Jikky0110@gmail.com>
2022-11-19 23:48:07 +0900

    Add ABC278

--- ABC/ABC250-299/ABC278/d.cpp
+++ ABC/ABC250-299/ABC278/d.cpp
@@ -6,26 +6,29 @@ int main(){
     cin.tie(nullptr);
     ios::sync_with_stdio(false);
     cout << fixed << setprecision(20);
-    int n,Q;
+    int n,q;
     cin >> n;
     vector<ll> a(n);
     for(int i = 0; i < n; i++) cin >> a[i];
-    cin >> Q;
+    cin >> q;
 
-    for(int _ = 0; _ < Q; _++){
-        int q; cin >> q;
-        if(q == 1){
-            int x; cin >> x;
-            vector<ll> b(n,x);
-            a = b;
+    ll base = 0;
+    map<int, ll> mp;
+    for(int i = 0; i < n; i++) mp[i] = a[i];
+
+    for(int _ = 0; _ < q; _++){
+        int type; cin >> type;
+        if(type == 1){
+            int x; cin >> x; base = x;
+            mp.clear();
         }
-        if(q == 2){
+        if(type == 2){
             int i, x; cin >> i >> x;
-            a[i-1] += x;
+            mp[i-1] += x;
         }
-        if(q == 3){
+        if(type == 3){
             int i; cin >> i;
-            cout << a[i-1] << endl;
+            cout << mp[i-1] + base << endl;
         }
     }
     return 0;
