c0d807677b716de8e5d659df71e6ecac22104d21
Jikky1618 <Jikky0110@gmail.com>
2022-11-24 22:47:29 +0900

    Commit

---
+++ ABC/ABC100-149/ABC112/a.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(20);
+    int n;
+    cin >> n;
+    if(n == 1){
+        cout << "Hello World" << endl;
+    }else{
+        int a,b;
+        cin >> a >> b;
+        cout << a + b << endl;
+    }
+    return 0;
+}
