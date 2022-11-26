6352a2e6439eb9c72068eb6682a60b9b94f9fc2b
Jikky1618 <Jikky0110@gmail.com>
2022-11-26 23:16:18 +0900

    Add ABC279

--- ABC/ABC250-299/ABC279/a.cpp
+++ ABC/ABC250-299/ABC279/a.cpp
@@ -8,9 +8,10 @@ int main(){
     cout << fixed << setprecision(20);
     string s;
     cin >> s;
+    int n = s.size();
 
     int ans = 0;
-    for(int i = 0; i < s.size(); i++){
+    for(int i = 0; i < n; i++){
         if(s[i] == 'v') ans++;
         if(s[i] == 'w') ans += 2;
     }
