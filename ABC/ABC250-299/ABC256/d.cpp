827328a6238a5a825fc6b08bafd3043ac4bfc317
Jikky1618 <Jikky0110@gmail.com>
2022-12-14 22:06:44 +0900

    Commit

--- ABC/ABC250-299/ABC256/d.cpp
+++ ABC/ABC250-299/ABC256/d.cpp
@@ -17,15 +17,10 @@ int main(){
     }
     for(int i = 0; i < MAX; i++) a[i + 1] += a[i];
 
-    int l = -1, r = -1;
-    for(int i = 0; i < MAX; i++){
-        if(a[i] == 0 && a[i + 1] != 0) l = i + 1;
-        if(a[i + 1] == 0 && a[i] != 0) r = i + 1;
-        if(l != -1 && r != -1){
-            cout << l << " " << r << endl;
-            l = -1, r = -1;
-        } 
-    }
+    for(int i = 1; i < MAX; i++){
+        if(a[i - 1] == 0 && a[i] != 0) cout << i << " ";
+        if(a[i - 1] != 0 && a[i] == 0) cout << i << endl;
+    } 
 
     return 0;
 }
