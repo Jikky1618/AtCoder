7f24f4cca46cfe8cb72c762b365ac63e96565436
Jikky1618 <Jikky0110@gmail.com>
2022-12-15 22:15:49 +0900

    Commit

--- ABC/ABC200-249/ABC228/c.cpp
+++ ABC/ABC200-249/ABC228/c.cpp
@@ -5,20 +5,24 @@ using ll = long long;
 int main(){
     cin.tie(nullptr);
     ios::sync_with_stdio(false);
-    int n,k;
-    cin >> n >> k;
-    k--;
-    vector<int> p1(n), p2(n),p3(n);
-    for(int i = 0; i < n; i++) cin >> p1[i] >> p2[i] >> p3[i];
+    cout << fixed << setprecision(20);
+    int N, K;
+    cin >> N >> K;
+    vector P(N, vector<int>(3));
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < 3; j++) cin >> P[i][j];
+    }
 
-    vector<int> s(n);
-    for(int i = 0; i < n; i++) s[i] = p1[i] + p2[i] + p3[i];
+    vector<int> s(N);
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < 3; j++) s[i] += P[i][j];
+    }
 
     vector<int> tmp = s;
-    sort(tmp.begin(), tmp.end(),greater<int>());
+    sort(tmp.begin(), tmp.end(), greater<int>());
 
-    for(int i = 0; i < n; i++){
-        if(s[i] + 300 >= tmp[k]) cout << "Yes" << endl;
+    for(int i = 0; i < N; i++){
+        if(tmp[K - 1] <= s[i] + 300) cout << "Yes" << endl;
         else cout << "No" << endl;
     }
     return 0;
