7b19d7171cc4d34a86a90ee4b2aed4456e66ddde
Jikky1618 <Jikky0110@gmail.com>
2022-12-18 04:58:20 +0900

    Commit

--- kyopro90/061.cpp
+++ kyopro90/061.cpp
@@ -5,16 +5,16 @@ using ll = long long;
 int main(){
     cin.tie(nullptr);
     ios::sync_with_stdio(false);
-    int q;
-    cin >> q;
-    vector<int> t(q),x(q);
-    for(int i = 0; i < q; i++) cin >> t[i] >> x[i];
+    cout << fixed << setprecision(20);
+    int Q;
+    cin >> Q;
 
     deque<int> deck;
-    for(int i = 0; i < q; i++){
-        if(t[i] == 1) deck.push_front(x[i]);
-        if(t[i] == 2) deck.push_back(x[i]);
-        if(t[i] == 3) cout << deck[x[i] - 1] << endl;
+    while(Q--){
+        int t, x; cin >> t >> x;
+        if(t == 1) deck.push_front(x);
+        if(t == 2) deck.push_back(x);
+        if(t == 3) cout << deck[x - 1] << endl;
     }
     return 0;
 }
