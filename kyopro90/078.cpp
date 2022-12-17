5d9c3d3cf46e710eb637c9ed601ebd5efee5f92d
Jikky1618 <Jikky0110@gmail.com>
2022-12-18 05:20:12 +0900

    Commit

--- kyopro90/078.cpp
+++ kyopro90/078.cpp
@@ -1,31 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-#define debug(x) cerr << "\033[33m[" << __LINE__ << "] " << #x << ": " << x << "\033[m" << endl;
 
 int main(){
     cin.tie(nullptr);
     ios::sync_with_stdio(false);
-    int n,m;
-    cin >> n >> m;
-    vector<vector<int>> Graph(n);
-    vector<int> a(m),b(m);
-    for(int i = 0; i < m; i++){
-        cin >> a[i] >> b[i];
-        a[i]--; b[i]--;
-        Graph[a[i]].push_back(b[i]);
-        Graph[b[i]].push_back(a[i]);
+    cout << fixed << setprecision(20);
+    int N, M;
+    cin >> N >> M;
+    vector<vector<int>> G(N);
+    for(int i = 0; i < M; i++){
+        int a, b; cin >> a >> b; a--, b--;
+        G[a].push_back(b);
+        G[b].push_back(a);
     }
 
-    int ans = 0;
-    for(int i = 0; i < n; i++){
+    int Ans = 0;
+    for(int i = 0; i < N; i++){
         int cnt = 0;
-        for(auto x: Graph[i]){
-            if(x < i) cnt++;
-        }
-        if(cnt == 1) ans++;
+        for(auto v: G[i]) if(v < i) cnt++;
+        if(cnt == 1) Ans++;
     }
 
-    cout << ans << endl;
+    cout << Ans << endl;
     return 0;
 }
