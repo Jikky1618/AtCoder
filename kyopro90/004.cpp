092abf3479594417b1248587b32d72b8598af75d
Jikky1618 <Jikky0110@gmail.com>
2022-12-18 04:00:55 +0900

    Commit

--- kyopro90/004.cpp
+++ kyopro90/004.cpp
@@ -1,35 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
 
-int main(void){
-    int h,w;
-    cin >> h >> w;
-
-    // Input
-    vector<vector<int>> grid(h,vector<int>(w));
-    for(int i = 0; i < h; i++){
-        for(int j = 0; j < w; j++){
-            cin >> grid[i][j];
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(20);
+    int H, W;
+    cin >> H >> W;
+    vector A(H, vector<int>(W));
+    for(int i = 0; i < H; i++){
+        for(int j = 0; j < W; j++){
+            cin >> A[i][j];
         }
     }
 
-    // Precalc
-    vector<int> Row(h);
-    vector<int> Column(w);
-    for(int i = 0; i < h; i++){
-        for(int j = 0; j < w; j++){
-            Row[i] += grid[i][j];
-            Column[j] += grid[i][j];
+    // 前計算
+    vector<int> h(H), w(W);
+    for(int i = 0; i < H; i++){
+        for(int j = 0; j < W; j++) h[i] += A[i][j];
+    }
+    for(int i = 0; i < W; i++){
+        for(int j = 0; j < H; j++) w[i] += A[j][i];
+    }
+
+    vector Ans(H, vector<int>(W));
+    for(int i = 0; i < H; i++){
+        for(int j = 0; j < W; j++){
+            Ans[i][j] = h[i] + w[j] - A[i][j];
         }
     }
-    
-    // Calc of the answer
-    //Output
-    vector<vector<int>> ans(h,vector<int>(w));
-    for(int i = 0; i < h; i++){
-        for(int j = 0; j < w; j++){
-            ans[i][j] = Row[i] + Column[j] - grid[i][j];
-            cout << ans[i][j] << " ";
+
+    for(int i = 0; i < H; i++){
+        for(int j = 0; j < W; j++){
+            cout << Ans[i][j] << " ";
         }
         cout << endl;
     }
