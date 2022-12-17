24ca6e1675c1a8a84a432caa2e634b70c00b9f74
Jikky1618 <Jikky0110@gmail.com>
2022-12-18 04:13:13 +0900

    Commit

--- kyopro90/010.cpp
+++ kyopro90/010.cpp
@@ -1,36 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
 
-int main(void){
-    
-    // Input
-    int n;
-    cin >> n;
-    vector<int> c(n),p(n);
-    for(int i = 0; i < n; i++) cin >> c[i] >> p[i];
-    int q;
-    cin >> q;
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(20);
+    int N;
+    cin >> N;
+    vector<int> C(N), P(N);
+    for(int i = 0; i < N; i++) cin >> C[i] >> P[i];
 
     // 累積和
-    vector<int> sum1(n + 1),sum2(n + 1);
-    for(int i = 0; i < n; i++){
-        if(c[i] == 1){
-            sum1[i + 1] = sum1[i] + p[i];
-            sum2[i + 1] = sum2[i];
-        }else{
-            sum1[i + 1] = sum1[i];
-            sum2[i + 1] = sum2[i] + p[i];
-        }
+    vector<int> sum1(N+1), sum2(N+1);
+    for(int i = 0; i < N; i++){
+        sum1[i + 1] = sum1[i];
+        sum2[i + 1] = sum2[i];
+        if(C[i] == 1) sum1[i + 1] += P[i];
+        if(C[i] == 2) sum2[i + 1] += P[i];
     }
 
-    // クエリ処理
-    for(int j = 0; j < q; j++){
-        int l,r; cin >> l >> r;
-        l--; // 半開区間にするため調整
-        int ans1 = sum1[r] - sum1[l]; 
-        int ans2 = sum2[r] - sum2[l];
-        cout << ans1 << " " << ans2 << endl;
+    int Q;
+    cin >> Q;
+    while(Q--){
+        int L, R; cin >> L >> R;
+        L--; // 半開区間にするため調整
+        cout << sum1[R] - sum1[L] << " " << sum2[R] - sum2[L] << endl;
     }
-
     return 0;
 }
