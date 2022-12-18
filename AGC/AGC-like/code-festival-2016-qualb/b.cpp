e78aee2602d83c6d133875065959a2fc3d94af6c
Jikky1618 <Jikky0110@gmail.com>
2022-12-18 17:15:27 +0900

    Add AGC

---
+++ AGC/AGC-like/code-festival-2016-qualb/b.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(20);
+    
+    return 0;
+}
