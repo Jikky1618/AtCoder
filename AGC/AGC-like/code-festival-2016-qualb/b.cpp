e492489eb29ee1789a3a6f34c6ee0ad3103ed122
Jikky1618 <Jikky0110@gmail.com>
2022-12-18 17:25:21 +0900

    Commit

--- AGC/AGC-like/code-festival-2016-qualb/b.cpp
+++ AGC/AGC-like/code-festival-2016-qualb/b.cpp
@@ -6,6 +6,27 @@ int main(){
     cin.tie(nullptr);
     ios::sync_with_stdio(false);
     cout << fixed << setprecision(20);
-    
+    int N, A, B;
+    string S;
+    cin >> N >> A >> B >> S;
+
+    int cnt = 0, cntB = 0;
+    for(int i = 0; i < N; i++){
+        if(S[i] == 'a'){
+            if(cnt < A + B){
+                cout << "Yes" << endl;
+                cnt++;
+            }
+            else cout << "No" << endl;
+        }
+        if(S[i] == 'b'){
+            if(cnt < A + B && cntB < B){
+                cout << "Yes" << endl;
+                cnt++, cntB++;
+            }
+            else cout << "No" << endl;
+        }
+        if(S[i] == 'c') cout << "No" << endl;
+    }
     return 0;
 }
