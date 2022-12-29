e3cc5bbee955aba59ac5773dffce73473eeae2aa
Jikky1618 <Jikky0110@gmail.com>
2022-12-29 11:22:50 +0900

    Commit

---
+++ ABC/ABC100-149/ABC100/a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(20);
+    int A, B;
+    cin >> A >> B;
+    
+    if(8 < A || 8 < B) cout << ":(" << endl;
+    else cout << "Yay!" << endl;
+    return 0;
+}
