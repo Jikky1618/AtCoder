d6f3b7386851d8717e609eb707372945ae897c88
Jikky1618 <Jikky0110@gmail.com>
2022-12-29 15:07:12 +0900

    Commit

---
+++ ABC/ABC050-099/ABC085/a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(20);
+    string S;
+    cin >> S;
+
+    S[3] = '8';
+    cout << S << endl;
+    return 0;
+}
