5e7040e2d3ebf015a6e15377965c39754905f60f
Jikky1618 <Jikky0110@gmail.com>
2022-12-29 15:59:33 +0900

    Commit

---
+++ ABC/ABC050-099/ABC069/a.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(20);
+    int n, m;
+    cin >> n >> m;
+    cout << (n - 1) * (m - 1) << endl;
+    return 0;
+}
