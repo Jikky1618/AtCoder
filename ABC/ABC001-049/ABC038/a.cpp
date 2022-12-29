2f8ddd756ca3d406b8064e1737005b690fae28a1
Jikky1618 <Jikky0110@gmail.com>
2022-12-29 18:56:06 +0900

    Commit

---
+++ ABC/ABC001-049/ABC038/a.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(20);
+    string S;
+    cin >> S;
+    cout << (S.back() == 'T' ? "YES" : "NO") << endl;
+    return 0;
+}
