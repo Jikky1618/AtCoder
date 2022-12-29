a36ff66feaab5d4bb32b092211cbfc317a961e63
Jikky1618 <Jikky0110@gmail.com>
2022-12-29 14:48:05 +0900

    Commit

--- ABC/ABC050-099/ABC096/b.cpp
+++ ABC/ABC050-099/ABC096/b.cpp
@@ -5,8 +5,18 @@ using ll = long long;
 int main(){
     cin.tie(nullptr);
     ios::sync_with_stdio(false);
-    int a,b,c,d;
-    cin >> a >> b >> c >> d;
-    cout << max(min(b,d) - max(a,c),0) << endl;
+    cout << fixed << setprecision(20);
+    vector<int> A(3);
+    for(int i = 0; i < 3; i++) cin >> A[i];
+    int K;
+    cin >> K;
+
+    sort(A.begin(), A.end(), greater<int>());
+    for(int i = 0; i < K; i++) A[0] *= 2;
+
+    int ans = 0;
+    for(int i = 0; i < 3; i++) ans += A[i];
+
+    cout << ans << endl;
     return 0;
 }
