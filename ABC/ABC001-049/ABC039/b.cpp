8ace3e509e06ce1b1ddcd8481920cccb010014a3
Jikky1618 <Jikky0110@gmail.com>
2022-12-29 18:55:06 +0900

    Commit

---
+++ ABC/ABC001-049/ABC039/b.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(20);
+    int X;
+    cin >> X;
+    int ans = sqrt(sqrt(X));
+    cout << ans << endl;
+    return 0;
+}
