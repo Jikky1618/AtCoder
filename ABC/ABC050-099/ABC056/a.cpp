ecc67875dd7708574c116e7f795afd5e6b0869d2
Jikky1618 <Jikky0110@gmail.com>
2022-12-29 17:10:27 +0900

    Commit

---
+++ ABC/ABC050-099/ABC056/a.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(20);
+    char a, b;
+    cin >> a >> b;
+    cout << (a == b ? 'H' : 'D') << endl;
+    return 0;
+}
