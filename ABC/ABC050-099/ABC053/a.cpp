ef22c7de6d797975499b6bfb09bd89e28fd5aa51
Jikky1618 <Jikky0110@gmail.com>
2022-12-29 17:20:31 +0900

    Commit

---
+++ ABC/ABC050-099/ABC053/a.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(20);
+    int x;
+    cin >> x;
+    cout << (x < 1200 ? "ABC" : "ARC") << endl;
+    return 0;
+}
