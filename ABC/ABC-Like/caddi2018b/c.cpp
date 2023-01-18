cad3209f036330b293dba7fd0a4a423d24ff66b1
Jikky1618 <Jikky0110@gmail.com>
2023-01-18 16:27:28 +0900

    Commit

---
+++ ABC/ABC-Like/caddi2018b/c.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+vector<pair<ll,ll>> prime_factorize(ll n){
+    vector<pair<ll, ll>> factor;
+    for (ll p = 2; p * p <= n; p++){
+        if (n % p != 0) continue;
+        ll exp = 0;
+        while (n % p == 0){ // 割れるまで割り続ける
+            exp++;
+            n /= p;
+        }
+        // 結果をpush
+        factor.push_back({p, exp});
+    }
+	if (n != 1) factor.push_back({n, 1});
+	return factor;
+}
+
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(20);
+    ll N, P;
+    cin >> N >> P;
+
+    auto prime = prime_factorize(P);
+    ll ans = 1;
+    for(auto&& [p, exp]: prime){
+        ans *= pow(p, (exp / N));
+    }
+
+    cout << ans << endl;
+}
