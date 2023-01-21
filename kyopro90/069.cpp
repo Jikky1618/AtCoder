32a486e1bc19de94b71edb7ba39cec5682d00f30
Jikky1618 <Jikky0110@gmail.com>
2023-01-22 08:52:29 +0900

    Add: 069.cpp

---
+++ kyopro90/069.cpp
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+template<int MOD> struct Mint {
+    ll val;
+    constexpr Mint(ll x = 0): val(x % MOD) {
+        if(val < 0) val += MOD;
+    }
+
+    constexpr Mint operator + () const {
+        return Mint(val);
+    }
+    constexpr Mint operator - () const {
+        return Mint(MOD - val);
+    }
+
+    constexpr Mint operator + (const Mint& x) const {
+        return Mint(*this) += x;
+    }
+    constexpr Mint operator - (const Mint& x) const {
+        return Mint(*this) -= x;
+    }
+    constexpr Mint operator * (const Mint& x) const {
+        return Mint(*this) *= x;
+    }
+    constexpr Mint operator / (const Mint& x) const {
+        return Mint(*this) /= x;
+    }
+
+    constexpr Mint& operator += (const Mint& x) {
+        if((val + x.val) >= MOD) val -= MOD;
+        return *this;
+    }
+    constexpr Mint& operator -= (const Mint& x) {
+        if((val += MOD - x.val) >= MOD) val -= MOD;
+        return *this;
+    }
+    constexpr Mint& operator *= (const Mint& x) {
+        (val *= x.val) %= MOD;
+        return *this;
+    }
+    constexpr Mint& operator /= (const Mint& x){
+        return (*this) *= x.inv();
+    }
+    constexpr bool operator == (const Mint& x) const {
+        return this->val == x.val;
+    }
+    constexpr bool operator != (const Mint& x) const {
+        return this->val != x.val;
+    }
+    
+    constexpr Mint pow(ll t) const {
+        if(!t) return 1;
+        Mint x = pow(t >> 1);
+        x *= x;
+        if(t & 1) x *= *this;
+        return x;
+    }
+
+    constexpr Mint inv() const {
+        ll a = val, b = MOD, u = 1, v = 0, t;
+        while(b > 0){
+            t = a / b;
+            swap(a -= t * b, b);
+            swap(u -= t * v, v);
+        }
+        return Mint(u);
+    }
+
+    friend constexpr istream& operator >> (istream& is, Mint& x){
+        is >> x.val;
+        x.val %= MOD;
+        if(x.val < 0) x.val += MOD;
+        return is;
+    }
+    friend constexpr ostream& operator << (ostream& os, const Mint& x){
+        return os << x.val;
+    }
+};
+
+const int MOD = 1e9 + 7;
+using mint = Mint<MOD>;
+
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(20);
+    ll N;
+    mint K;
+    cin >> N >> K;
+
+    mint ans;
+    if(K == 1){
+        ans = (N == 1 ? 1 : 0);
+    }else if(N == 1){
+        ans = K;
+    }else if(N == 2){
+        ans = K * (K - 1);
+    }else{
+        ans = K * (K - 1) * (K - 2).pow(N - 2);
+    }
+
+    cout << ans << endl;
+}
