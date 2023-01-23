a5de7689386777ee17ca32a740f1af7d87074867
Jikky <Jikky0110@gmail.com>
2023-01-23 19:17:12 +0900

    Add: c.cpp

---
+++ ABC/ABC100-149/ABC101/c.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#ifdef LOCAL
+#include <debug_print.hpp>
+#define debug(...) debug_print::multi_print(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...) (static_cast<void>(0))
+#endif
+
+template<class T>
+T ceil(T x, T y){
+    return (x + y - 1) / y;
+}
+
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(20);
+    int N, K;
+    cin >> N >> K;
+    vector<int> A(N);
+    for(int i = 0; i < N; i++) cin >> A[i];
+
+    int ans = ceil(N - 1, K - 1);
+    cout << ans << endl;
+}
