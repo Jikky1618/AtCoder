8b28d515010964888a6396491091c2d3caa4fdc8
Jikky <Jikky0110@gmail.com>
2023-01-25 20:39:10 +0900

    Add: d.cpp

---
+++ ABC/ABC150-199/ABC167/d.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+#ifdef LOCAL
+#include <debug_print.hpp>
+#define debug(...) debug_print::multi_print(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...) (static_cast<void>(0))
+#endif
+
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(20);
+    ll N, K;
+    cin >> N >> K;
+    vector<int> A(N);
+    for(int i = 0; i < N; i++) cin >> A[i];
+
+    vector<int> cycle(N + 1, -1);
+    vector<int> s;
+    int pos = 1, cycle_cnt = 0;
+    while(cycle[pos] == -1){ // まだ到達していないか
+        cycle[pos] = s.size();
+        s.push_back(pos);
+        pos = A[pos - 1];
+    }
+    cycle_cnt = s.size() - cycle[pos];
+
+    if(K < cycle[pos]){
+        cout << s[K] << endl;
+    }else{
+        K = (K - cycle[pos]) % cycle_cnt;
+        cout << s[K + cycle[pos]] << endl;
+    }
+}
