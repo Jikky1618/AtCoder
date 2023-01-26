a9194e7d2a7e20505585404574d414070fcd370e
Jikky <Jikky0110@gmail.com>
2023-01-26 14:07:07 +0900

    Add: b.cpp

--- ABC/ABC200-249/ABC225/b.cpp
+++ ABC/ABC200-249/ABC225/b.cpp
@@ -2,25 +2,31 @@
 using namespace std;
 using ll = long long;
 
+#ifdef LOCAL
+#include <debug_print.hpp>
+#define debug(...) debug_print::multi_print(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...) (static_cast<void>(0))
+#endif
+
 int main(){
-    int n;
-    cin >> n;
-    vector<int> count(n + 1);
-    
-    int a,b;
-    for(int i = 1; i < n; i++){
-        cin >> a >> b;
-        count[a]++;
-        count[b]++;
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(20);
+    int N;
+    cin >> N;
+    vector<vector<int>> G(N);
+    for(int i = 0; i < N - 1; i++){
+        int a, b; cin >> a >> b, a--, b--;
+        G[a].push_back(b);
+        G[b].push_back(a);
     }
 
-    for(int i = 1; i <= n; i++){
-        if(count[i] == n - 1){
-            cout << "Yes" << endl;
-            return 0;
-        }
+    bool star = false;
+    for(int i = 0; i < N; i++){
+        int deg = G[i].size();
+        if(deg == N - 1) star = true;
     }
-    cout << "No" << endl;
 
-    return 0;
+    cout << (star ? "Yes" : "No") << endl;
 }
