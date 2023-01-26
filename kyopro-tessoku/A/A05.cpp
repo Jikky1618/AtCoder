3b7e6f0a006a5a1843c781d794b71069c2d052d6
Jikky <Jikky0110@gmail.com>
2023-01-26 17:43:01 +0900

    Add: 05

--- kyopro-tessoku/A/A05.cpp
+++ kyopro-tessoku/A/A05.cpp
@@ -2,20 +2,27 @@
 using namespace std;
 using ll = long long;
 
+#ifdef LOCAL
+#include <debug_print.hpp>
+#define debug(...) debug_print::multi_print(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...) (static_cast<void>(0))
+#endif
+
 int main(){
     cin.tie(nullptr);
     ios::sync_with_stdio(false);
-    int n,k;
-    cin >> n >> k;
+    cout << fixed << setprecision(20);
+    int N, K;
+    cin >> N >> K;
 
     int ans = 0;
-    for(int red = 1; red <= n; red++){
-        for(int blue = 1; blue <= n; blue++){
-            int white = k - red - blue;
-            if(1 <= white && white <= n) ans++;
+    for(int i = 1; i <= N; i++){
+        for(int j = 1; j <= N; j++){
+            int k = K - i - j;
+            if(1 <= k && k <= N) ans++;
         }
     }
 
     cout << ans << endl;
-    return 0;
 }
