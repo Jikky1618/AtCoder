9b368273364158d4fce48a107f54f5c1a74ee900
Jikky <Jikky0110@gmail.com>
2023-01-30 20:37:57 +0900

    Update: A13

--- kyopro-tessoku/A/A13.cpp
+++ kyopro-tessoku/A/A13.cpp
@@ -2,28 +2,29 @@
 using namespace std;
 using ll = long long;
 
+#ifdef LOCAL
+#include <debug_print.hpp>
+#define debug(...) debug_print::multi_print(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...) (static_cast<void>(0))
+#endif
+
 int main(){
     cin.tie(nullptr);
     ios::sync_with_stdio(false);
-    ll n,k;
-    cin >> n >> k;
-    vector<ll> a(n);
-    for(int i = 0; i < n; i++) cin >> a[i];
+    cout << fixed << setprecision(20);
+    int N, K;
+    cin >> N >> K;
+    vector<int> A(N);
+    for(int i = 0; i < N; i++) cin >> A[i];
 
-    // しゃくとり法
-    vector<int> r(n);
-    for(int i = 0; i < n; i++){
-        if(i == 0) r[i] = 0;
-        else r[i] = r[i-1];
-        // ギリギリまで増やす
-        while(r[i] < n-1 && a[r[i]+1]-a[i] <= k){
-            r[i]++;
-        }
+    int right = 0;
+    int ans = 0;
+    // しゃくとり法: leftを全探索
+    for(int left = 0; left < N; left++){
+        while(right < N && A[right] - A[left] <= K) right++;
+        ans += right - left - 1;
     }
 
-    ll ans = 0;
-    for(int i = 0; i < n; i++) ans += r[i] - i;
-
     cout << ans << endl;
-    return 0;
 }
