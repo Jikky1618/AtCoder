d48f715462e4bab0f1aa75700f5ac23bc9a58ba8
Jikky <Jikky0110@gmail.com>
2023-01-30 19:40:43 +0900

    Add: A11

---
+++ kyopro-tessoku/A/A11_meguru.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+#ifdef LOCAL
+#include <debug_print.hpp>
+#define debug(...) debug_print::multi_print(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...) (static_cast<void>(0))
+#endif
+
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(20);
+    int N, X;
+    cin >> N >> X;
+    vector<int> A(N);
+    for(int i = 0; i < N; i++) cin >> A[i];
+
+    sort(A.begin(), A.end());
+
+    auto judge = [&](int mid){
+        return (X <= A[mid]);
+    };
+    
+    int ok = A.size(), ng = -1;
+    while(ok - ng > 1){
+        int mid = (ok + ng) / 2;
+        if(judge(mid)) ok = mid;
+        else ng = mid;
+    }
+
+    cout << ok + 1 << endl;
+}
