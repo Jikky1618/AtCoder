bf6c02fbf026436f8d1ddfd3fcd00f491f171805
Jikky <Jikky0110@gmail.com>
2023-02-03 19:07:44 +0900

    Update: A22

--- kyopro-tessoku/A/A22.cpp
+++ kyopro-tessoku/A/A22.cpp
@@ -3,6 +3,13 @@ using namespace std;
 using ll = long long;
 const int INF = (1 << 30) - 1;
 
+#ifdef LOCAL
+#include <debug_print.hpp>
+#define debug(...) debug_print::multi_print(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...) (static_cast<void>(0))
+#endif
+
 int main(){
     cin.tie(nullptr);
     ios::sync_with_stdio(false);
@@ -10,17 +17,17 @@ int main(){
     int N;
     cin >> N;
     vector<int> A(N - 1), B(N - 1);
-    for(int i = 0; i < N - 1; i++) cin >> A[i];
-    for(int i = 0; i < N - 1; i++) cin >> B[i];
+    for(int i = 0; i < N - 1; i++) cin >> A[i], A[i]--;
+    for(int i = 0; i < N - 1; i++) cin >> B[i], B[i]--;
 
-    // dp[i] := マスiまでの合計スコアの最大値
-    vector<int> dp(N + 1, -INF);
-    dp[1] = 0;
-
+    // dp[i] := マスiにたどり着いたとき合計スコアの最大値
+    vector<int> dp(N, -INF);
+    dp[0] = 0;
     for(int i = 0; i < N - 1; i++){
-        dp[A[i]] = max(dp[A[i]], dp[i+1] + 100);
-        dp[B[i]] = max(dp[B[i]], dp[i+1] + 150);
+        dp[A[i]] = max(dp[A[i]], dp[i] + 100);
+        dp[B[i]] = max(dp[B[i]], dp[i] + 150);
     }
 
-    cout << dp[N] << endl;
+    debug(dp);
+    cout << dp[N - 1] << endl;
 }
