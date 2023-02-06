876f71a99fdbeeb766da985b6be8c4478aa241d2
Jikky <Jikky0110@gmail.com>
2023-02-06 10:43:14 +0900

    Update: 076.cpp

--- kyopro90/076.cpp
+++ kyopro90/076.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
+
 #ifdef LOCAL
 #include <debug_print.hpp>
 #define debug(...) debug_print::multi_print(#__VA_ARGS__, __VA_ARGS__)
@@ -14,7 +15,7 @@ int main(){
     cout << fixed << setprecision(20);
     int N;
     cin >> N;
-    vector<ll> A(2 * N);
+    vector<ll> A(N);
     for(int i = 0; i < N; i++) cin >> A[i];
 
     // 累積和
@@ -22,7 +23,21 @@ int main(){
     for(int i = 0; i < N; i++) S[i + 1] = S[i] + A[i];
     for(int i = 0; i < N; i++) S[N + i + 1] = S[N + i] + A[i];
 
-    for(int l = 0; l < 2 * N; l++){
-        while()
+    auto val = [&](int l, int r) -> ll {
+        return S[r] - S[l];
+    };
+
+    ll sum = val(0, N);
+    // 区間をしゃくとり法
+    int r = 0;
+    for(int l = 0; l < N; l++){
+        r = max(r, l + 1);
+        while(r < 2 * N && val(l, r) < sum / 10) r++;
+        if(val(l, r) ==  sum / 10){
+            cout << "Yes" << endl;
+            return 0;
+        }
     }
+
+    cout << "No" << endl;
 }
