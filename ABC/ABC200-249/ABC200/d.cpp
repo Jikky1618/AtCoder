188f2ba68dfd80fa0cc959d75bc5f922dab617df
Jikky <Jikky0110@gmail.com>
2023-02-08 22:36:43 +0900

    Add: d.cpp

--- ABC/ABC200-249/ABC200/d.cpp
+++ ABC/ABC200-249/ABC200/d.cpp
@@ -1,43 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-const int MAX = 1e6;
 
-vector<bool> make_is_prime(ll x){
-    vector<bool> prime(x + 1, true);
-    prime[0] = prime[1] = false;
-    for(int i = 2; i * i <= x; i++){
-        if(!prime[i]) continue;
-        for(int j = i * 2; j <= x; j += i){
-            prime[j] = false; // iの倍数を削除
-        }
-    }
-    return prime;
-}
+#ifdef LOCAL
+#include <debug_print.hpp>
+#define debug(...) debug_print::multi_print(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...) (static_cast<void>(0))
+#endif
 
 int main(){
     cin.tie(nullptr);
     ios::sync_with_stdio(false);
     cout << fixed << setprecision(20);
-    ll N;
+    int N;
     cin >> N;
-
-    auto is_prime = make_is_prime(MAX);
-
-    // MAXまでの素数のみを格納
-    vector<ll> primes;
-    for(int i = 0; i < MAX; i++) if(is_prime[i]) primes.push_back(i);
-
-    // 累積和
-    vector<ll> s(MAX+1);
-    for(auto&& p: primes) s[p] = 1;
-    for(int i = 0; i < MAX; i++) s[i + 1] += s[i];
-
-    ll ans = 0;
-    for(auto&& q: primes){
-        ll pmax = min(N / q / q / q, q - 1);
-        ans += s[pmax]; 
+    vector<int> A(N);
+    for(int i = 0; i < N; i++) cin >> A[i];
+    
+    vector<vector<int>> C(200, vector<int>(0));
+    // 高々201通りを全探索
+    for(int bit = 0; bit < (1 << min(N, 8)); bit++){
+        vector<int> B;
+        int M = 0;
+        for(int i = 0; i < min(N, 8); i++){
+            if((bit >> i) & 1) B.push_back(i + 1), M = (M + A[i]) % 200;
+        }
+        if(C[M] != vector<int>(0)){
+            cout << "Yes" << endl;
+            cout << B.size() << " ";
+            for(auto b: B){
+                cout << b << " ";
+            }
+            cout << endl;
+            cout << C[M].size() << " ";
+            for(auto c: C[M]){
+                cout << c << " ";
+            }
+            cout << endl;
+            return 0;
+        }else{
+            C[M] = B;
+        }
     }
 
-    cout << ans << endl;
+    cout << "No" << endl;
 }
