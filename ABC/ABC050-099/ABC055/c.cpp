56af9d3d8cd6ff67e33cce9547acaf029b70ff00
Jikky <Jikky0110@gmail.com>
2023-02-18 14:13:30 +0900

    Add: c.cpp

---
+++ ABC/ABC050-099/ABC055/c.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+#ifdef LOCAL
+#include <debug_print.hpp>
+#define debug(...) debug_print::multi_print(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...) (static_cast<void>(0))
+#endif
+
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(20);
+    ll N, M;
+    cin >> N >> M;
+
+    ll ans = 0;
+    if(2 * N <= M){
+        ans = N + (M - 2 * N) / 4;
+    }else{
+        ans = M / 2;
+    }
+
+    cout << ans << endl;
+}
