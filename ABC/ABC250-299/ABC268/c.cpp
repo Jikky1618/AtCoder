6f0efc137fd6331384256f8a7063945eb4cc5809
Jikky <Jikky0110@gmail.com>
2023-02-28 05:01:16 +0900

    Update: c.cpp

--- ABC/ABC250-299/ABC268/c.cpp
+++ ABC/ABC250-299/ABC268/c.cpp
@@ -2,25 +2,31 @@
 using namespace std;
 using ll = long long;
 
+#ifdef LOCAL
+#include <debug_print.hpp>
+#define debug(...) debug_print::multi_print(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...) (static_cast<void>(0))
+#endif
+
 int main(){
     cin.tie(nullptr);
     ios::sync_with_stdio(false);
-    int n;
-    cin >> n;
-    vector<int> p(n);
-    for(int i = 0; i < n; i++) cin >> p[i];
+    int N;
+    cin >> N;
+    vector<int> p(N);
+    for(int i = 0; i < N; i++) cin >> p[i];
 
     // 何回回したら何人喜ぶかのカウント用配列
-    vector<int> count(n);
-    for(int i = 0; i < n; i++){
+    vector<int> cnt(N);
+    for(int i = 0; i < N; i++){
         for(int j = -1; j <= 1; j++){
-            // 料理Piがiを喜ばせる配置ならカウント
-            count[(p[i]-i+j+n)%n]++;
+            // cnt回回すと、料理P[i]がiを喜ばせる配置になる
+            cnt[(p[i] - i + j + N) % N]++;
         }
     }
-    sort(count.begin(), count.end(),greater<int>());
 
-    cout << count[0] << endl;
+    cout << *max_element(cnt.begin(), cnt.end()) << endl;
 
     return 0;
 }
