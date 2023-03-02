4d6b20754ab7a9dab921ad534f98c066daa22290
Jikky <Jikky0110@gmail.com>
2023-03-02 22:05:56 +0900

    Update: ABC178

--- ABC/ABC150-199/ABC178/d.cpp
+++ ABC/ABC150-199/ABC178/d.cpp
@@ -9,12 +9,23 @@ using ll = long long;
 #define debug(...) (static_cast<void>(0))
 #endif
 
+const int MOD = 1e9 + 7;
+
 int main(){
     cin.tie(nullptr);
     ios::sync_with_stdio(false);
     cout << fixed << setprecision(20);
-    ll a, b, c, d;
-    cin >> a >> b >> c >> d;
-    
-    cout << max({a * c, a * d, b * c, b * d}) << endl;
+    int S;
+    cin >> S;
+
+    // dp[i] := 総和がiになるような数列の数
+    vector<ll> dp(S + 1);
+    dp[0] = 1;
+    for(int i = 0; i < S; i++){
+        for(int j = 3; j <= S; j++){
+            if(i + j <= S) dp[i + j] += dp[i], dp[i + j] %= MOD;
+        }
+    }
+
+    cout << dp[S] << endl;
 }
