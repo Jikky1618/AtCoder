dd5ee4a91357c69b7a7ceab59e3d90679c3993a2
Jikky <Jikky0110@gmail.com>
2023-03-07 23:24:50 +0900

    Update: ABC206

--- ABC/ABC200-249/ABC206/c.cpp
+++ ABC/ABC200-249/ABC206/c.cpp
@@ -1,20 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
+using ll = long long;
 
-int main(void){
-    int n;
-    cin >> n;
-    vector<int> a(n);
-    for(int i = 0; i < n; i++) cin >> a[i];
+#ifdef LOCAL
+#include <debug_print.hpp>
+#define debug(...) debug_print::multi_print(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...) (static_cast<void>(0))
+#endif
 
-    map<int,int> map;
-    ll ans = 0;
-    for(int j = 0; j < n; j++){
-        ans += j - map[a[j]];
-        map[a[j]]++;
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(20);
+    ll N;
+    cin >> N;
+    vector<int> A(N);
+    for(int i = 0; i < N; i++) cin >> A[i];
+
+    map<int, ll> mp;
+    for(int i = 0; i < N; i++) mp[A[i]]++;
+
+    // 余事象を考える
+    ll ans = N * (N - 1) / 2;
+    for(auto [key, val]: mp){
+        ans -= val * (val - 1) / 2;
     }
-    cout << ans << endl;
 
-    return 0;
+    cout << ans << endl;
 }
