72a09246ee027996913b89ddac7fdeea6ac40854
Jikky <Jikky0110@gmail.com>
2023-03-08 03:36:37 +0900

    Update: a.cpp

--- ABC/ABC200-249/ABC206/a.cpp
+++ ABC/ABC200-249/ABC206/a.cpp
@@ -1,13 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
 
-int main(void){
-    int n;
-    cin >> n;
-    int ans = floor(1.08 * n);
-    if(ans < 206) cout << "Yay!" << endl;
-    else if(ans == 206) cout << "so-so" << endl;
-    else cout << ":(" << endl;
+#ifdef LOCAL
+#include <debug_print.hpp>
+#define debug(...) debug_print::multi_print(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...) (static_cast<void>(0))
+#endif
 
-    return 0;
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(20);
+    int N;
+    cin >> N;
+
+    int val = N * 108 / 100;
+    if(val < 206) cout << "Yay!" << endl;
+    if(val == 206) cout << "so-so" << endl;
+    if(val > 206) cout << ":(" << endl;
 }
