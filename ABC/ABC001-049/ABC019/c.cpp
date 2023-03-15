f1ec84214105f9796b420c5a1fe2992ab7644f62
Jikky <Jikky0110@gmail.com>
2023-03-16 00:24:31 +0900

    Add: c.cpp

---
+++ ABC/ABC001-049/ABC019/c.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+#ifdef LOCAL
+#include <debug_print.hpp>
+#define debug(...) debug_print::multi_print(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...) (static_cast<void>(0))
+#endif
+
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(20);
+    int N;
+    cin >> N;
+    vector<int> A(N);
+    for(int i = 0; i < N; i++) cin >> A[i];
+
+    for(int i = 0; i < N; i++){
+        while(A[i] % 2 == 0) A[i] /= 2;
+    }
+    
+    set<int> st;
+    for(int i = 0; i < N; i++) st.insert(A[i]);
+
+    cout << st.size() << endl;
+}
