8c34bee41e1c567331a32d1768153d6ee895ea6c
Jikky <Jikky0110@gmail.com>
2023-03-24 11:03:28 +0900

    Add: e.cpp

---
+++ ABC/ABC200-249/ABC218/e.cpp
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+#ifdef LOCAL
+#include <debug_print.hpp>
+#define debug(...) debug_print::multi_print(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...) (static_cast<void>(0))
+#endif
+
+struct UnionFind {
+    int n, group_size;
+    vector<int> parent_or_size;
+
+    UnionFind(int size): n(size), group_size(size), parent_or_size(size, -1) {}
+
+    int root(int x){
+        return (parent_or_size[x] < 0 ? x : parent_or_size[x] = root(parent_or_size[x]));
+    }
+
+    bool same(int x, int y){
+        return root(x) == root(y);
+    }
+
+    bool merge(int x, int y){
+        int rx = root(x), ry = root(y);
+        if(rx == ry) return false;
+        if(-parent_or_size[rx] < -parent_or_size[ry]) swap(rx, ry); 
+        parent_or_size[rx] += parent_or_size[ry]; 
+        parent_or_size[ry] = rx;
+        group_size--;
+        return true;
+    }
+
+    int size(int x){
+        return -parent_or_size[root(x)];
+    }
+
+    int group_count(){
+        return group_size;
+    }
+
+    vector<int> group(int x){
+        vector<int> res;
+        for(int i = 0; i < n; i++){
+            if(same(x, i)) res.push_back(i);
+        }
+        return res;
+    }
+
+    vector<vector<int>> groups(){
+        vector<vector<int>> res(n);
+        for(int i = 0; i < n; i++){
+            res[root(i)].push_back(i);
+        }
+        res.erase(remove_if(res.begin(), res.end(), [&](const vector<int>& v){
+            return v.empty();
+        }), res.end());
+        return res;
+    }
+};
+
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(20);
+    int N, M;
+    cin >> N >> M;
+
+    ll sum = 0;
+    // {cost, from, to}
+    vector<tuple<int, int, int>> G;
+    for(int i = 0; i < M; i++){
+        int A, B, C; cin >> A >> B >> C, A--, B--;
+        G.emplace_back(C, A, B);
+        sum += C;
+    }
+
+
+    sort(G.begin(), G.end());
+    UnionFind uf(N);
+
+    ll ans = sum;
+    // 罰金を最小化させる最小全域木
+    for(auto [cost, from, to]: G){
+        if(cost <= 0){
+            uf.merge(from, to);
+            ans -= cost;
+        }else if(!uf.same(from, to)){
+            uf.merge(from, to);
+            ans -= cost;
+        }
+    }
+
+    cout << ans << endl;
+}
