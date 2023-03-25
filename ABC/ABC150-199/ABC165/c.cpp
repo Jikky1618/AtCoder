3a75b9a98e95e7f9f3c477b6938ffc621d93b54c
Jikky <Jikky0110@gmail.com>
2023-03-25 19:12:45 +0900

    Update: c.cpp

--- ABC/ABC150-199/ABC165/c.cpp
+++ ABC/ABC150-199/ABC165/c.cpp
@@ -2,34 +2,41 @@
 using namespace std;
 using ll = long long;
 
-int n,m,q;
-vector<int> a(q),b(q),c(q),d(q);
-vector<int> A(10);
-int ans = 0;
-// cnt: 何回目の再帰関数か。
-// last: A[cnt] の要素はなにか。
-void dfs(int cnt = 0, int last = 1){
-    if(cnt == n){
-        int sum = 0;
-        for(int i = 0; i < q; i++){
-            if(A[b[i] - 1] - A[a[i] - 1] == c[i]) sum += d[i];
-        }
-        ans = max(ans, sum);
-        return;
-    }
-    for(int i = last; i <= m; i++){
-        A[cnt] = i;
-        dfs(cnt + 1, i);
-    }
-}
+#ifdef LOCAL
+#include <debug_print.hpp>
+#define debug(...) debug_print::multi_print(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...) (static_cast<void>(0))
+#endif
 
 int main(){
-    cin.tie(nullptr);
-    ios::sync_with_stdio(false);
-    cin >> n >> m >> q;
-    a.resize(q); b.resize(q); c.resize(q); d.resize(q);
-    for(int i = 0; i < q; i++) cin >> a[i] >> b[i] >> c[i] >> d[i];
-    dfs();
-    cout << ans << endl;
-    return 0;
+	cin.tie(nullptr);
+	ios::sync_with_stdio(false);
+	cout << fixed << setprecision(20);
+	int N, M, Q;
+	cin >> N >> M >> Q;
+	vector<int> a(Q), b(Q), c(Q), d(Q);
+	for(int i = 0; i < Q; i++) cin >> a[i] >> b[i] >> c[i] >> d[i];
+
+	int ans = 0;
+	vector<int> A;
+	auto dfs = [&](auto &&self, int pos, int last) -> void {
+		if(pos == N){
+			int val = 0;
+			for(int i = 0; i < Q; i++){
+				if(A[b[i] - 1] - A[a[i] - 1] == c[i]) val += d[i];
+			}
+			ans = max(ans, val);
+			return;
+		}
+
+		for(int i = last; i <= M; i++){
+			A.push_back(i);
+			self(self, pos + 1, i);
+			A.pop_back();
+		}
+	};
+
+	dfs(dfs, 0, 1);
+	cout << ans << endl;
 }
