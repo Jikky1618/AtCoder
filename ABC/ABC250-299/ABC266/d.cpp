f7c9efe5cd9200da716990a42899cb9ca7bae988
Jikky <Jikky0110@gmail.com>
2023-03-29 08:45:29 +0900

    Update: d.cpp

--- ABC/ABC250-299/ABC266/d.cpp
+++ ABC/ABC250-299/ABC266/d.cpp
@@ -1,38 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-const ll LINF = 1LL << 60;
-#define M 100005
+
+#ifdef LOCAL
+#include <debug_print.hpp>
+#define debug(...) debug_print::multi_print(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...) (static_cast<void>(0))
+#endif
+
+const ll INF = 1LL << 60;
+const int MAX = 1e5 + 1;
 
 int main(){
-    cin.tie(nullptr);
-    ios::sync_with_stdio(false);
-    cout << fixed << setprecision(20);
-    int n;
-    cin >> n;
-    // 入力は時間ベース
-    vector<ll> x(M),a(M);
-    for(int i = 0; i < n; i++){
-        ll T,X,A; cin >> T >> X >> A;
-        x[T] = X;
-        a[T] = A;
-    }
+	cin.tie(nullptr);
+	ios::sync_with_stdio(false);
+	cout << fixed << setprecision(20);
+	int N;
+	cin >> N;
+	vector<int> T(N), X(N), A(N);
+	for(int i = 0; i < N; i++) cin >> T[i] >> X[i] >> A[i];
 
-    // dp[i][j] := 時刻iに穴jの上にいるときの、大きさの最大値
-    vector<vector<ll>> dp(M,vector<ll>(5,-LINF));
-    dp[0][0] = 0;
-    for(int i = 0; i < M-1; i++){
-        for(int j = 0; j < 5; j++){
-            dp[i + 1][j] = max(dp[i + 1][j], dp[i][j]);
-            if(j != 4) dp[i + 1][j] = max(dp[i + 1][j], dp[i][j + 1]);
-            if(j != 0) dp[i + 1][j] = max(dp[i + 1][j], dp[i][j - 1]);
-        }
-        dp[i+1][x[i+1]] += a[i+1];
-    }
+	// 時間基準にする
+	vector<pair<int, int>> p(MAX);
+	for(int i = 0; i < N; i++){
+		p[T[i]] = {X[i], A[i]};
+	}
 
-    ll ans = -LINF;
-    for(int i = 0; i < 5; i++) ans = max(ans, dp[M-1][i]);
+	// dp[i][j] := 時刻iに座標jにいるときの大きさの合計の最大値
+	vector dp(MAX + 1, vector<ll>(5, -INF));
+	dp[0][0] = 0;
 
-    cout << ans << endl;
-    return 0;
+	for(int i = 0; i < MAX; i++){
+		dp[i][p[i].first] += p[i].second;
+		for(int j = 0; j < 5; j++){
+			if(0 < j) dp[i + 1][j] = max(dp[i + 1][j], dp[i][j - 1]);
+			if(j < 4) dp[i + 1][j] = max(dp[i + 1][j], dp[i][j + 1]);
+			dp[i + 1][j] = max(dp[i + 1][j], dp[i][j]);
+		}
+	}
+
+	ll ans = *max_element(dp[MAX].begin(), dp[MAX].end());
+	cout << ans << endl;
 }
