98125bc66a948f6d3255987a5f5bab8e67e10e93
Jikky <Jikky0110@gmail.com>
2023-03-31 21:47:57 +0900

    Update: d.cpp

--- ABC/ABC150-199/ABC162/d.cpp
+++ ABC/ABC150-199/ABC162/d.cpp
@@ -4,30 +4,55 @@ using ll = long long;
 
 #ifdef LOCAL
 #include <debug_print.hpp>
-#define debug(...) debug_print::multi_print(#__VA_ARGS__, __VA_ARGS__)
 #else
-#define debug(...) (static_cast<void>(0))
 #endif
 
 int main(){
-	cin.tie(nullptr);
-	ios::sync_with_stdio(false);
-	cout << fixed << setprecision(20);
-	ll X;
-	cin >> X;
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(20);
+    int N;
+    string S;
+    cin >> N >> S;
 
-	map<ll, ll> mp; // val^5 = key
-	// Bのありえる範囲は-200~200
-	for(ll i = -200; i <= 200; i++){
-		mp[i * i * i * i * i] = i;
-	}
+    auto naive = [&]() -> void {
+        int ans = 0;
+        for(int i = 0; i < N; i++){
+            for(int j = i + 1; j < N; j++){
+                for(int k = j + 1; k < N; k++){
+                    if(S[i] == S[j] || S[j] == S[k] || S[k] == S[i]) continue;
+                    if(j - i != k - j){
+                        ans++;
+                    }
+                }
+            }
+        }
+        cout << ans << endl;
+    };
 
-	// {B^5, B}の値を全探索
-	for(auto [B5, B]: mp){
-		if(mp.count(X + B5)){
-			ll A = mp[X + B5]; // A^5 = X + B^5
-			cout << A << " " << B << endl;
-			return 0;
-		}
-	}
+    const string col = "RGB";
+    // 各文字ごとに累積和
+    vector acc(3, vector<int>(N + 1));
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < N; j++){
+            acc[i][j + 1] = acc[i][j] + (S[j] == col[i]);
+        }
+    }
+
+    ll ans = 0;
+    // j, kを全探索
+    for(int j = 0; j < N; j++){
+        for(int k = j + 1; k < N; k++){
+            if(S[j] == S[k]) continue;
+            // num := S[i]でもS[j]でない色
+            int num = 0;
+            for(int i = 0; i < 3; i++) if(col[i] != S[j] && col[i] != S[k]) num = i;
+
+            // i < j を満たすiでS[i] = col[num]の数をカウント
+            ll cnt = acc[num][j] - (2 * j - k < N && S[2 * j - k] == col[num]);
+            ans += cnt;
+        }
+    }
+
+    cout << ans << endl;
 }
