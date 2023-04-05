3eb6ef4a3833159cb9d3921939c7770e51614017
Jikky <Jikky0110@gmail.com>
2023-04-06 03:34:47 +0900

    Update: d.cpp

--- ABC/ABC100-149/ABC138/d.cpp
+++ ABC/ABC100-149/ABC138/d.cpp
@@ -2,34 +2,40 @@
 using namespace std;
 using ll = long long;
 
-void dfs(int pos, int p, vector<vector<int>> &graph, vector<int> &res){
-    if(p != -1) res[pos] += res[p];
-    for(auto &&npos: graph[pos]){
-        if(npos == p) continue;
-        dfs(npos, pos, graph, res);
-    }
-}
+#ifdef LOCAL
+#include <debug_print.hpp>
+#define debug(...) debug_print::multi_print(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...) (static_cast<void>(0))
+#endif
 
 int main(){
     cin.tie(nullptr);
     ios::sync_with_stdio(false);
-    int n,q;
-    cin >> n >> q;
-    vector<vector<int>> graph(n);
-    for(int i = 0; i < n - 1; i++){
-        int a,b; cin >> a >> b; a--; b--;
-        graph[a].push_back(b);
-        graph[b].push_back(a);
-    }
-    // imos法
-    vector<int> sum(n);
-    for(int i = 0; i < q; i++){
-        int p,x; cin >> p >> x; p--;
-        sum[p] += x;
+    cout << fixed << setprecision(20);
+    int N, Q;
+    cin >> N >> Q;
+    vector<vector<int>> G(N);
+    for(int i = 0; i < N - 1; i++){
+        int a, b; cin >> a >> b, a--, b--;
+        G[a].emplace_back(b);
+        G[b].emplace_back(a);
     }
 
-    dfs(0, -1, graph, sum);
-    for(auto &&i: sum) cout << i << " ";
-    cout << endl;
-    return 0;
+    vector<int> ans(N);
+    while(Q--){
+        int p, x; cin >> p >> x, p--;
+        ans[p] += x;
+    }
+
+    auto dfs = [&](auto &&self, int pos, int par) -> void {
+        for(auto np: G[pos]){
+            if(np == par) continue;
+            ans[np] += ans[pos]; // imos法
+            self(self, np, pos);
+        }
+    };
+    dfs(dfs, 0, -1);
+    
+    for(int i = 0; i < N; i++) cout << ans[i] << " \n"[i == N - 1];
 }
