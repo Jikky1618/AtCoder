630acde62277bcf69c00b10d035279196b85df09
Jikky <Jikky0110@gmail.com>
2023-04-11 23:25:14 +0900

    Update: d.cpp

--- ABC/ABC250-299/ABC267/d.cpp
+++ ABC/ABC250-299/ABC267/d.cpp
@@ -1,29 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-const ll LINF = 1LL << 60;
+const ll INF = 1LL << 60;
+
+#ifdef LOCAL
+#include <debug_print.hpp>
+#define debug(...) debug_print::multi_print(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...) (static_cast<void>(0))
+#endif
 
 int main(){
     cin.tie(nullptr);
     ios::sync_with_stdio(false);
     cout << fixed << setprecision(20);
-    int n,m;
-    cin >> n >> m;
-    vector<ll> a(n);
-    for(int i = 0; i < n; i++) cin >> a[i];
+    int N, M;
+    cin >> N >> M;
+    vector<ll> A(N);
+    for(int i = 0; i < N; i++) cin >> A[i];
 
-    // dp[i][j] := A_iまでのうち、j個,Bの要素として選んだときの最大値
-    vector<vector<ll>> dp(n+1,vector<ll>(m+1,-LINF));
+    // dp[i][j] := A[i]まででj個選んだときの最大値
+    vector dp(N + 1, vector<ll>(M + 1, -INF));
     dp[0][0] = 0;
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j <= m; j++){
-            // 選ばない場合
-            dp[i+1][j] = max(dp[i+1][j], dp[i][j]);
-            // 選ぶ場合
-            if(j > 0) dp[i+1][j] = max(dp[i+1][j], dp[i][j-1]+a[i]*j);
+
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j <= M; j++){
+            // A[i]を選ばないとき
+            dp[i + 1][j] = max(dp[i + 1][j], dp[i][j]);
+            // A[i]を選ぶとき
+            if(j + 1 <= M) dp[i + 1][j + 1] = max(dp[i + 1][j + 1], dp[i][j] + (j + 1) * A[i]);
         }
     }
-
-    cout << dp[n][m] << endl;
-    return 0;
+    
+    cout << dp[N][M] << endl;
 }
