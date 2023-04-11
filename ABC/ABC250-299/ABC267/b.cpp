330dffd6fd8a2258997df7c09035ad0ec111c235
Jikky <Jikky0110@gmail.com>
2023-04-11 22:20:40 +0900

    Update: b.cpp

--- ABC/ABC250-299/ABC267/b.cpp
+++ ABC/ABC250-299/ABC267/b.cpp
@@ -2,36 +2,50 @@
 using namespace std;
 using ll = long long;
 
+#ifdef LOCAL
+#include <debug_print.hpp>
+#define debug(...) debug_print::multi_print(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...) (static_cast<void>(0))
+#endif
+
 int main(){
-    string s;
-    cin >> s;
-    if(s[0] == '1'){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(20);
+    string S;
+    cin >> S;
+
+    if(S[0] == '1'){
         cout << "No" << endl;
         return 0;
     }
-    vector<bool> column(7);
-    column[0] = (s[6] == '1');
-    column[1] = (s[3] == '1');
-    column[2] = (s[1] == '1' || s[7] == '1');
-    column[3] = (s[4] == '1');
-    column[4] = (s[2] == '1' || s[8] == '1');
-    column[5] = (s[5] == '1');
-    column[6] = (s[9] == '1');
+
+    vector<int> c(7);
+    c[0] = (S[6] == '1');
+    c[1] = (S[3] == '1');
+    c[2] = (S[1] == '1' || S[7] == '1');
+    c[3] = (S[0] == '1' || S[4] == '1');
+    c[4] = (S[2] == '1' || S[8] == '1');
+    c[5] = (S[5] == '1');
+    c[6] = (S[9] == '1');
 
     for(int i = 0; i < 7; i++){
-        for(int j = 0; j < i; j++){
-            if(column[i] && column[j]){
-                for(int k = j + 1; k < i; k++){
-                    if(!column[k]){
-                        cout << "Yes" << endl;
-                        return 0;
-                    }
+        for(int j = i + 2; j < 7; j++){
+            // 列i, jが立っているとき
+            if(c[i] && c[j]){
+                bool flag = true;
+                // 間の列が全て倒れているかを確認
+                for(int k = i + 1; k < j; k++){
+                    if(c[k] == 1) flag = false;
+                }
+                if(flag){
+                    cout << "Yes" << endl;
+                    return 0;
                 }
             }
         }
     }
 
     cout << "No" << endl;
-
-    return 0;
 }
