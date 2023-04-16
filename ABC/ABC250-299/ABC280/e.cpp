17b3d2d10a2cbf1de381a2b2249dc9c63de26b2c
Jikky <Jikky0110@gmail.com>
2023-04-16 18:10:27 +0900

    Add: e.cpp

---
+++ ABC/ABC250-299/ABC280/e.cpp
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+#ifdef LOCAL
+#include <debug_print.hpp>
+#define debug(...) debug_print::multi_print(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...) (static_cast<void>(0))
+#endif
+
+template<int MOD> struct Mint {
+    ll val;
+    constexpr Mint(ll x = 0): val(x % MOD) {
+        if(val < 0) val += MOD;
+    }
+    constexpr int getmod() const { return MOD; }
+    constexpr Mint operator + () const {
+        return Mint(val);
+    }
+    constexpr Mint operator - () const {
+        return Mint(MOD - val);
+    }
+    constexpr Mint operator + (const Mint& x) const {
+        return Mint(*this) += x;
+    }
+    constexpr Mint operator - (const Mint& x) const {
+        return Mint(*this) -= x;
+    }
+    constexpr Mint operator * (const Mint& x) const {
+        return Mint(*this) *= x;
+    }
+    constexpr Mint operator / (const Mint& x) const {
+        return Mint(*this) /= x;
+    }
+    constexpr Mint& operator += (const Mint& x) {
+        if((val += x.val) >= MOD) val -= MOD;
+        return *this;
+    }
+    constexpr Mint& operator -= (const Mint& x) {
+        if((val += MOD - x.val) >= MOD) val -= MOD;
+        return *this;
+    }
+    constexpr Mint& operator *= (const Mint& x) {
+        (val *= x.val) %= MOD;
+        return *this;
+    }
+    constexpr Mint& operator /= (const Mint& x){
+        return (*this) *= x.inv();
+    }
+    constexpr bool operator == (const Mint& x) const {
+        return this->val == x.val;
+    }
+    constexpr bool operator != (const Mint& x) const {
+        return this->val != x.val;
+    }
+    constexpr Mint pow(ll t) const {
+        if(t == 0) return 1;
+        Mint x = pow(t >> 1);
+        x *= x;
+        if(t & 1) x *= *this;
+        return x;
+    }
+    constexpr Mint inv() const {
+        ll a = val, b = MOD, u = 1, v = 0;
+        while(b > 0){
+            ll t = a / b;
+            swap(a -= t * b, b);
+            swap(u -= t * v, v);
+        }
+        return Mint(u);
+    }
+    friend constexpr istream& operator >> (istream& is, Mint& x) {
+        is >> x.val;
+        x.val %= MOD;
+        if(x.val < 0) x.val += MOD;
+        return is;
+    }
+    friend constexpr ostream& operator << (ostream& os, const Mint& x) {
+        return os << x.val;
+    }
+};
+
+const int MOD = 998244353;
+using mint = Mint<MOD>;
+
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(20);
+    int N, P;
+    cin >> N >> P;
+
+    // dp[i] := 体力がiのときの攻撃回数の期待値
+    vector<mint> dp(N + 1, 0);
+    dp[0] = 0, dp[1] = 1;
+
+    mint p_inv = mint(P) / mint(100);
+    for(int i = 2; i <= N; i++){
+        dp[i] += (dp[i - 2] + 1) * p_inv;
+        dp[i] += (dp[i - 1] + 1) * (mint(1) - p_inv);
+    }
+
+    cout << dp[N] << endl;
+}
