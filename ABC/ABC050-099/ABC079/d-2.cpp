6f5665dd6c90fbe961907c4cd3f0c1aa06d0e80b
Jikky <Jikky0110@gmail.com>
2023-05-02 18:56:23 +0900

    Add: d.cpp

--- ABC/ABC050-099/ABC079/d-2.cpp
+++ ABC/ABC050-099/ABC079/d-2.cpp
@@ -9,6 +9,36 @@ using ll = long long;
 #define debug(...) (static_cast<void>(0))
 #endif
 
+const int INF = (1 << 30) - 1;
+template<class T>
+bool warsahll_floyd(vector<vector<pair<int, T>>>& G, vector<vector<ll>>& dist){
+    int n = G.size();
+    dist = vector(n, vector<ll>(n, INF));
+    for(int from = 0; from < n; from++){
+        for(auto [to, cost]: G[from]){
+            dist[from][to] = cost;
+        }
+    }
+    for(int i = 0; i < n; i++){
+        dist[i][i] = 0;
+    }
+
+    for(int i = 0; i < n; i++){
+        for(int to = 0; to < n; to++){
+            for(int from = 0; from < n; from++){
+                if(dist[from][i] == INF || dist[i][to] == INF) continue;
+                dist[from][to] = min(dist[from][to], dist[from][i] + dist[i][to]);
+            }
+        }
+    }
+
+    // 負の閉路検知
+    for(int i = 0; i < n; i++){
+        if(dist[i][i] < 0) return false;
+    }
+    return true;
+}
+
 int main(){
     cin.tie(nullptr);
     ios::sync_with_stdio(false);
@@ -24,21 +54,22 @@ int main(){
         for(int j = 0; j < W; j++) cin >> A[i][j];
     }
 
-    // ワーシャルフロイド法
-    auto G = c;
+    // グラフに変える
+    vector<vector<pair<int, int>>> G(10);
     for(int i = 0; i < 10; i++){
-        for(int to = 0; to < 10; to++){
-            for(int from = 0; from < 10; from++){
-                G[from][to] = min(G[from][to], G[from][i] + G[i][to]);
-            }
+        for(int j = 0; j < 10; j++){
+            G[i].emplace_back(j, c[i][j]);
         }
     }
+
+    vector<vector<ll>> dist;
+    warsahll_floyd(G, dist);
 
     int ans = 0;
     for(int i = 0; i < H; i++){
         for(int j = 0; j < W; j++){
             if(A[i][j] == -1 || A[i][j] == 1) continue;
-            ans += G[A[i][j]][1];
+            ans += dist[A[i][j]][1];
         }
     }
 
