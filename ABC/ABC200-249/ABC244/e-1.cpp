d90ad546d5361e8793574de6a753c0b5f620ddf8
Jikky <Jikky0110@gmail.com>
2023-05-05 17:24:32 +0900

    Add: e.cpp

---
+++ ABC/ABC200-249/ABC244/e-1.cpp
@@ -0,0 +1,119 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+#ifdef LOCAL
+#include <debug_print.hpp>
+#define debug(...) debug_print::multi_print(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...) (static_cast<void>(0))
+#endif
+
+template<int MOD> struct Mint {
+    ll val;
+    constexpr Mint(ll x = 0): val(x % MOD) {
+        if(val < 0) val += MOD;
+    }
+    constexpr int getmod() const { return MOD; }
+    constexpr Mint operator + () const {
+        return Mint(val);
+    }
+    constexpr Mint operator - () const {
+        return Mint(MOD - val);
+    }
+    constexpr Mint operator + (const Mint& x) const {
+        return Mint(*this) += x;
+    }
+    constexpr Mint operator - (const Mint& x) const {
+        return Mint(*this) -= x;
+    }
+    constexpr Mint operator * (const Mint& x) const {
+        return Mint(*this) *= x;
+    }
+    constexpr Mint operator / (const Mint& x) const {
+        return Mint(*this) /= x;
+    }
+    constexpr Mint& operator += (const Mint& x) {
+        if((val += x.val) >= MOD) val -= MOD;
+        return *this;
+    }
+    constexpr Mint& operator -= (const Mint& x) {
+        if((val += MOD - x.val) >= MOD) val -= MOD;
+        return *this;
+    }
+    constexpr Mint& operator *= (const Mint& x) {
+        (val *= x.val) %= MOD;
+        return *this;
+    }
+    constexpr Mint& operator /= (const Mint& x){
+        return (*this) *= x.inv();
+    }
+    constexpr bool operator == (const Mint& x) const {
+        return this->val == x.val;
+    }
+    constexpr bool operator != (const Mint& x) const {
+        return this->val != x.val;
+    }
+    constexpr Mint pow(ll t) const {
+        if(t == 0) return 1;
+        Mint x = pow(t >> 1);
+        x *= x;
+        if(t & 1) x *= *this;
+        return x;
+    }
+    constexpr Mint inv() const {
+        ll a = val, b = MOD, u = 1, v = 0;
+        while(b > 0){
+            ll t = a / b;
+            swap(a -= t * b, b);
+            swap(u -= t * v, v);
+        }
+        return Mint(u);
+    }
+    friend constexpr istream& operator >> (istream& is, Mint& x) {
+        is >> x.val;
+        x.val %= MOD;
+        if(x.val < 0) x.val += MOD;
+        return is;
+    }
+    friend constexpr ostream& operator << (ostream& os, const Mint& x) {
+        return os << x.val;
+    }
+};
+
+const int MOD = 998244353;
+using mint = Mint<MOD>;
+
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(20);
+    int N, M, K, S, T, X;
+    cin >> N >> M >> K >> S >> T >> X, S--, T--, X--;
+    vector<vector<int>> G(N);
+    for(int i = 0; i < M; i++){
+        int U, V; cin >> U >> V, U--, V--;
+        G[U].emplace_back(V);
+        G[V].emplace_back(U);
+    }
+
+    // dp[i][j][k] := i回目で頂点jに訪れたとき, Xにk(mod 2)回訪れたときの場合の数
+    vector dp(K + 1, vector(N, vector<mint>(2)));
+    dp[0][S][0] = 1;
+
+    for(int i = 0; i < K; i++){
+        for(int j = 0; j < N; j++){
+            for(auto np: G[j]){
+                for(int k = 0; k < 2; k++){
+                    if(np == X){
+                        dp[i + 1][np][(k + 1) % 2] += dp[i][j][k];
+                    }else{
+                        dp[i + 1][np][k] += dp[i][j][k];
+                    }
+                }
+            }
+        }
+    }
+
+    cout << dp[K][T][0] << endl;
+}
