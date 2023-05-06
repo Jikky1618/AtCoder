a4d4849019e6dbb6c4d6380bf8d152bdaf485864
Jikky <Jikky0110@gmail.com>
2023-05-06 13:22:12 +0900

    Add: d.cpp

---
+++ ABC/ABC100-149/ABC132/d.cpp
@@ -0,0 +1,130 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+#ifdef LOCAL
+#include <debug_print.hpp>
+#define debug(...) debug_print::multi_print(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...) (static_cast<void>(0))
+#endif
+
+template<int MOD> struct Mint {
+    ll val;
+    constexpr Mint(ll x = 0): val(x % MOD) {
+        if(val < 0) val += MOD;
+    }
+    constexpr int getmod() const { return MOD; }
+    constexpr Mint operator + () const {
+        return Mint(val);
+    }
+    constexpr Mint operator - () const {
+        return Mint(MOD - val);
+    }
+    constexpr Mint operator + (const Mint& x) const {
+        return Mint(*this) += x;
+    }
+    constexpr Mint operator - (const Mint& x) const {
+        return Mint(*this) -= x;
+    }
+    constexpr Mint operator * (const Mint& x) const {
+        return Mint(*this) *= x;
+    }
+    constexpr Mint operator / (const Mint& x) const {
+        return Mint(*this) /= x;
+    }
+    constexpr Mint& operator += (const Mint& x) {
+        if((val += x.val) >= MOD) val -= MOD;
+        return *this;
+    }
+    constexpr Mint& operator -= (const Mint& x) {
+        if((val += MOD - x.val) >= MOD) val -= MOD;
+        return *this;
+    }
+    constexpr Mint& operator *= (const Mint& x) {
+        (val *= x.val) %= MOD;
+        return *this;
+    }
+    constexpr Mint& operator /= (const Mint& x){
+        return (*this) *= x.inv();
+    }
+    constexpr bool operator == (const Mint& x) const {
+        return this->val == x.val;
+    }
+    constexpr bool operator != (const Mint& x) const {
+        return this->val != x.val;
+    }
+    constexpr Mint pow(ll t) const {
+        if(t == 0) return 1;
+        Mint x = pow(t >> 1);
+        x *= x;
+        if(t & 1) x *= *this;
+        return x;
+    }
+    constexpr Mint inv() const {
+        ll a = val, b = MOD, u = 1, v = 0;
+        while(b > 0){
+            ll t = a / b;
+            swap(a -= t * b, b);
+            swap(u -= t * v, v);
+        }
+        return Mint(u);
+    }
+    friend constexpr istream& operator >> (istream& is, Mint& x) {
+        is >> x.val;
+        x.val %= MOD;
+        if(x.val < 0) x.val += MOD;
+        return is;
+    }
+    friend constexpr ostream& operator << (ostream& os, const Mint& x) {
+        return os << x.val;
+    }
+};
+
+const int MOD = 1e9 + 7;
+using mint = Mint<MOD>;
+
+template<class T> struct Binomial {
+    vector<T> fact_, inv_, finv_;
+    constexpr Binomial(int n) noexcept: fact_(n, 1), inv_(n), finv_(n) {
+        init(n);
+    }
+    constexpr void init(int n) noexcept {
+        fact_.assign(n, 1), inv_.assign(n, 1), finv_.assign(n, 1);
+        for(int i = 2; i < n; i++){
+            fact_[i] = fact_[i - 1] * i;
+            inv_[i] = -inv_[MOD % i] * (MOD / i);
+            finv_[i] = finv_[i - 1] * inv_[i];
+        }
+    }
+    constexpr T com(int n, int r) const noexcept {
+        if(n < r || n < 0 || r < 0) return 0;
+        return fact_[n] * finv_[r] * finv_[n - r];
+    }
+    constexpr T fact(int n) const noexcept {
+        if(n < 0) return 0;
+        return fact_[n];
+    }
+    constexpr T inv(int n) const noexcept {
+        if(n < 0) return 0;
+        return inv[n];
+    }
+    constexpr T finv(int n) const noexcept {
+        if(n < 0) return 0;
+        return finv_[n];
+    }
+};
+
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(20);
+    int N, K;
+    cin >> N >> K;
+
+    Binomial<mint> BC(2000);
+    for(int i = 1; i <= K; i++){
+        mint ans = BC.com(K - 1, i - 1) * BC.com(N - K + 1, i);
+        cout << ans << "\n";
+    }
+}
