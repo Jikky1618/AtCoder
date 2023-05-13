b2ba2abadb7f6b1008a9c24f472dea6340d7a706
Jikky <Jikky0110@gmail.com>
2023-05-14 00:56:30 +0900

    Update: b.cpp

--- ABC/ABC250-299/ABC255/b.cpp
+++ ABC/ABC250-299/ABC255/b.cpp
@@ -1,31 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-const int INF = 1e9;
+const ll INF = 1LL << 60;
+
+#ifdef LOCAL
+#include <debug_print.hpp>
+#define debug(...) debug_print::multi_print(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...) (static_cast<void>(0))
+#endif
 
 int main(){
-    int n,k;
-    cin >> n >> k;
-    vector<int> a(k);
-    for(int i = 0; i < k; i++) cin >> a[i];
-    vector<int> x(n),y(n);
-    for(int i = 0; i < n; i++) cin >> x[i] >> y[i];
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(20);
+    int N, K;
+    cin >> N >> K;
+    vector<int> A(K);
+    for(int i = 0; i < K; i++) cin >> A[i], A[i]--;
+    vector<int> X(N), Y(N);
+    for(int i = 0; i < N; i++) cin >> X[i] >> Y[i];
 
-    double res = 0;
-    for(int i = 0; i < n; i++){
-            double min = INF,dist_x,dist_y,dist;
-        for(int j = 0; j < k; j++){
-            dist_x = x[i] - x[a[j] - 1];
-            dist_y = y[i] - y[a[j] - 1];
-            dist = sqrt(dist_x * dist_x + dist_y * dist_y);
-            if(min > dist) min = dist;
+    ll ans = 0;
+    for(int i = 0; i < N; i++){
+        ll val = INF;
+        for(int j = 0; j < K; j++){
+            ll x = X[i] - X[A[j]], y = Y[i] - Y[A[j]];
+            ll dist = x * x + y * y;
+            val = min(val, dist);
         }
-        if(res < min) res = min;
+        ans = max(ans, val);
     }
 
-    printf("%lf",res);
-
-    return 0;
+    cout << sqrt(ans) << endl;
 }
-
-
