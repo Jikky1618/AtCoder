039b4dd57292484c81e0fe64aef777b5a5dbfce4
Jikky <Jikky0110@gmail.com>
2023-05-28 04:58:03 +0900

    Update: c.cpp

--- ABC/ABC100-149/ABC114/c.cpp
+++ ABC/ABC100-149/ABC114/c.cpp
@@ -2,27 +2,37 @@
 using namespace std;
 using ll = long long;
 
-void find_753(int n, ll num, int used, int &cnt){
-    if(num > n) return; // nより大きいと終了
+#ifdef LOCAL
+#include <debug_print.hpp>
+#define debug(...) debug_print::multi_print(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...) (static_cast<void>(0))
+#endif
 
-    bool flag = true;
-    for(int i = 0; i < 3; i++){
-        if(!((used >> i) & 1)) flag = false;
-    }
-    if(flag) cnt++;
-
-    find_753(n, num * 10 + 7, used | (1 << 2), cnt);
-    find_753(n, num * 10 + 5, used | (1 << 1), cnt);
-    find_753(n, num * 10 + 3, used | (1 << 0), cnt);
-}
 int main(){
     cin.tie(nullptr);
     ios::sync_with_stdio(false);
-    int n;
-    cin >> n;
-    
+    cout << fixed << setprecision(20);
+    int N;
+    cin >> N;
+
     int ans = 0;
-    find_753(n, 0, 0, ans);
-    cout << ans <<endl;
-    return 0;
+    auto dfs = [&](auto&& self, ll val, vector<int> used) -> void {
+        if(N < val) return;
+
+        bool flag = true;
+        for(int i = 0; i < 3; i++){
+            if(used[i] == 0) flag = false;
+        }
+
+        if(flag) ans++;
+        for(int i = 0; i < 3; i++){
+            used[i]++;
+            self(self, val * 10 + vector<int>{7, 5, 3}[i], used);
+            used[i]--;
+        }
+    };
+
+    dfs(dfs, 0, {0, 0, 0});
+    cout << ans << endl;
 }
