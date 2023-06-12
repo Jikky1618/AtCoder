ca352d5f39d44133f97eba639f2cdc19f7f23630
Jikky <Jikky0110@gmail.com>
2023-06-12 20:00:46 +0900

    Add: NIKKEI2019-2-QUAL

---
+++ ARC/ARC-Like/NIKKEI2019-2-QUAL/a.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+#ifdef LOCAL
+#include <debug_print.hpp>
+#define debug(...) debug_print::multi_print(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...) (static_cast<void>(0))
+#endif
+
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(20);
+    int N;
+    cin >> N;
+
+    int ans = 0;
+    for(int i = 1; i < N; i++){
+        int j = N - i;
+        if(i >= j) break;
+        ans++;
+    }
+
+    cout << ans << endl;
+}
