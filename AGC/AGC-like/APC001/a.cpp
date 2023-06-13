477a60230c80c7120a6a3f8371bc09e22579d887
Jikky <Jikky0110@gmail.com>
2023-06-14 04:20:02 +0900

    Add: a.cpp

---
+++ AGC/AGC-like/APC001/a.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+#ifdef LOCAL
+#include <debug_print.hpp>
+#define debug(...) debug_print::multi_print(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...) (static_cast<void>(0))
+#endif
+
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(20);
+    int X, Y;
+    cin >> X >> Y;
+
+    cout << (X % Y == 0 ? -1 : X) << endl;
+}
