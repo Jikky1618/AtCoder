5734742b1d0552f89a1ee378a1a61bb06db5e2f5
Jikky <Jikky0110@gmail.com>
2023-06-19 22:26:46 +0900

    Add: a.cpp

--- ARC/ARC100-149/ARC146/a.cpp
+++ ARC/ARC100-149/ARC146/a.cpp
@@ -1,26 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
+
+#ifdef LOCAL
+#include <debug_print.hpp>
+#define debug(...) debug_print::multi_print(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...) (static_cast<void>(0))
+#endif
 
 int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(20);
     int N;
     cin >> N;
-    vector<string> num(N);
-    for(int i = 0; i < N; i++) cin >> num[i];
+    vector<string> A(N);
+    for(int i = 0; i < N; i++) cin >> A[i];
 
-    sort(num.begin(),num.end(),greater<string>());
-
-    int max = 0,value = 0;
-    for(int i = 0; i < 3; i++){
-        for(int j = 0; j < 3; j++){
-            int k = 3 - i - j;
-            if(i != j){
-                value = stoi(num[i] + num[j] + num[k]);
-                if(value > max) max = value;
-            }
-        }
+    // 文字桁数で分ける
+    vector<vector<string>> V(8);
+    for(int i = 0; i < N; i++){
+        int M = A[i].size();
+        V[M].emplace_back(A[i]);
     }
 
-    cout << max << endl;
+    debug(V);
+    vector<string> S;
+    int cnt = 0;
+    for(int i = 7; i >= 0; i--){
+        if(V[i].empty()) continue;
+        sort(V[i].begin(), V[i].end(), greater<string>());
+        for(auto T: V[i]){
+            if(cnt == 3) break;
+            cnt++;
+            S.emplace_back(T);
+        }
+        if(cnt == 3) break;
+    }
 
-    return 0;
+    ll ans = 0;
+    vector<int> P{0, 1, 2};
+    do{
+        string val = S[P[0]] + S[P[1]] + S[P[2]];
+        ans = max(ans, stoll(val));
+    }while(next_permutation(P.begin(), P.end()));
+    
+    cout << ans << endl;
 }
