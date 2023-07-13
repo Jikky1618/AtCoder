adc221d451790dc7dd6097a5e40d95783d975edd
Jikky <Jikky0110@gmail.com>
2023-07-13 17:59:29 +0900

    Add: b.cpp

--- ARC/ARC050-099/ARC050/b.cpp
+++ ARC/ARC050-099/ARC050/b.cpp
@@ -19,16 +19,14 @@ int main(){
     auto check = [&](ll mid) -> bool {
         // mid 組作るとき, 必ず両方 mid 本を消費
         ll r = R - mid, b = B - mid;
-
-        ll val = 0;
-        if(r > 0) val += r / (x - 1);
-        if(b > 0) val += b / (y - 1);
+        ll val = r / (x - 1) + b / (y - 1);
         debug(val, mid);
         return r >= 0 && b >= 0 && val >= mid;
     };
 
     // mid 組以上作れるかで二分探索
-    ll ok = -1, ng = ll(1e18) + 1;
+    // ng := 花の本数の合計の半分以上は組が作れない
+    ll ok = -1, ng = (R + B) / 2 + 1;
     while(ng - ok > 1){
         ll mid = (ok + ng) / 2;
         (check(mid) ? ok : ng) = mid;
