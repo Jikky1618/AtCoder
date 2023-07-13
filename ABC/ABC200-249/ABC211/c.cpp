10463c0f5b3255d36f8c089f214c865f71a77e9c
Jikky <Jikky0110@gmail.com>
2023-07-14 01:24:23 +0900

    Update: c.cpp

--- ABC/ABC200-249/ABC211/c.cpp
+++ ABC/ABC200-249/ABC211/c.cpp
@@ -1,32 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
+
+#ifdef LOCAL
+#include <debug_print.hpp>
+#define debug(...) debug_print::multi_print(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...) (static_cast<void>(0))
+#endif
+
 const int MOD = 1e9 + 7;
- 
+
 int main(){
     cin.tie(nullptr);
     ios::sync_with_stdio(false);
-    string s;
-    cin >> s;
- 
-    int n = s.size();
-    string t = "chokudai";
-    // dp[i][j] := Sのi文字目までにchokudaiのj文字目まで選択する方法
-    vector<vector<int>> dp(n + 1,vector<int>(9));
- 
-    // chokudaiの0文字目までの選択は下線をなにも引いていない状態(1通り)
-    for(int i = 0; i < n + 1; i++) dp[i][0] = 1;
- 
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < 8; j++){
-            if(s[i] == t[j]){
-                dp[i + 1][j + 1] = (dp[i][j + 1] + dp[i][j]) % MOD;
-            }else{
-                dp[i + 1][j + 1] = dp[i][j + 1] % MOD;
-            }
+    cout << fixed << setprecision(20);
+    string S;
+    cin >> S;
+
+    const string T = "chokudai";
+    int N = S.size(), M = T.size();
+    // dp[i][j] := S の i 文字目までを見て, chokudai の j 文字目までを選択するパターン数
+    vector dp(N + 1, vector<ll>(M + 1));
+    dp[0][0] = 1;
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j <= M; j++){
+            // S[i] が T[j] のとき, S[i] を選ぶときの遷移
+            if(j < M && S[i] == T[j]) dp[i + 1][j + 1] += dp[i][j], dp[i + 1][j + 1] %= MOD;
+            // S[i] を選ばないときの遷移
+            dp[i + 1][j] += dp[i][j], dp[i + 1][j] %= MOD;
         }
     }
- 
-    cout << dp[n][8] << endl;
-    return 0;
+
+    cout << dp[N][M] << endl;
 }
