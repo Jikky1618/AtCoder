d77aae908dcc8323e0423961ad12467a8b338d13
Jikky <Jikky0110@gmail.com>
2023-07-16 02:12:20 +0900

    Update: B26.cpp

--- kyopro-tessoku/B/B26.cpp
+++ kyopro-tessoku/B/B26.cpp
@@ -1,16 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-#define debug(x) cerr << "\033[33m[" << __LINE__ << "] " << #x << ": " << x << "\033[m" << endl;
 
-vector<bool> makeIsPrime(ll x){
-    vector<bool> prime(x + 1, true);
-    if(0 <= x) prime[0] = false;
-    if(1 <= x) prime[1] = false;
-    for(int i = 2; i * i <= x; i++){
-        if(!prime[i]) continue;
-        for(int j = i * 2; j <= x; j += i){
-            prime[j] = false; // iの倍数を削除
+#ifdef LOCAL
+#include <debug_print.hpp>
+#define debug(...) debug_print::multi_print(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...) (static_cast<void>(0))
+#endif
+
+// エラトラネスの篩で N 以下の素数を求める
+vector<bool> make_is_prime(int N) {
+    vector<bool> prime(N + 1, true);
+    prime[0] = prime[1] = false;
+    for (int i = 2; i * i <= N; i++) {
+        if (!prime[i]) continue;
+        for (int j = i * i; j <= N; j += i) {
+            prime[j] = false;
         }
     }
     return prime;
@@ -19,11 +25,11 @@ vector<bool> makeIsPrime(ll x){
 int main(){
     cin.tie(nullptr);
     ios::sync_with_stdio(false);
-    int n;
-    cin >> n;
-    vector<bool> ans = makeIsPrime(n);
-    for(int i = 0; i <= n; i++){
-        if(ans[i]) cout << i << endl;
+    cout << fixed << setprecision(20);
+    int N;
+    cin >> N;
+    auto p = make_is_prime(N);
+    for(int i = 1; i <= N; i++){
+        if(p[i]) cout << i << '\n';
     }
-    return 0;
 }
