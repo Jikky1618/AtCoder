d269cc4569edb9cf098d1c2576176b7cf69abbba
Jikky <Jikky0110@gmail.com>
2023-07-18 17:47:27 +0900

    Update: a.cpp

--- ARC/ARC001-049/ARC004/a.cpp
+++ ARC/ARC001-049/ARC004/a.cpp
@@ -2,23 +2,29 @@
 using namespace std;
 using ll = long long;
 
+#ifdef LOCAL
+#include <debug_print.hpp>
+#define debug(...) debug_print::multi_print(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...) (static_cast<void>(0))
+#endif
+
 int main(){
     cin.tie(nullptr);
     ios::sync_with_stdio(false);
     cout << fixed << setprecision(20);
-    int n;
-    cin >> n;
-    vector<double> x(n), y(n);
-    for(int i = 0; i < n; i++) cin >> x[i] >> y[i];
+    int N;
+    cin >> N;
+    vector<int> x(N), y(N);
+    for(int i = 0; i < N; i++) cin >> x[i] >> y[i];
 
     double ans = 0;
-    for(int i = 0; i < n; i++){
-        for(int j = i + 1; j < n; j++){
-            double dist = hypot(x[i] - x[j], y[i] - y[j]);
+    for(int i = 0; i < N; i++){
+        for(int j = i + 1; j < N; j++){
+            double dist = hypot(abs(x[i] - x[j]), abs(y[i] - y[j]));
             ans = max(ans, dist);
         }
     }
 
     cout << ans << endl;
-    return 0;
 }
