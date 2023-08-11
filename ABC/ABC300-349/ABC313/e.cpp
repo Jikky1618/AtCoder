514428a6cac2dcc13c8bb6aba4125f0d8c88e4e3
Jikky <Jikky0110@gmail.com>
2023-08-11 21:19:37 +0900

    Add: e.cpp

---
+++ ABC/ABC300-349/ABC313/e.cpp
@@ -0,0 +1,126 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+#ifdef LOCAL
+#include <debug_print.hpp>
+#define debug(...) debug_print::multi_print(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...) (static_cast<void>(0))
+#endif
+
+template<int MOD> struct modint {
+    ll val;
+    constexpr modint(ll x = 0): val(x % MOD) {
+        if(val < 0) val += MOD;
+    }
+    constexpr int mod() const { return MOD; }
+    constexpr modint operator + () const {
+        return modint(val);
+    }
+    constexpr modint operator - () const {
+        return modint(MOD - val);
+    }
+    constexpr modint& operator++() {
+        return (*this) += 1;
+    }
+    constexpr modint& operator--() {
+        return (*this) -= 1;
+    }
+    constexpr modint operator++(int) {
+        modint x = (*this); ++(*this);
+        return x;
+    }
+    constexpr modint operator--(int) {
+        modint x = (*this); --(*this);
+        return x;
+    }
+    constexpr modint operator + (const modint& x) const {
+        return modint(*this) += x;
+    }
+    constexpr modint operator - (const modint& x) const {
+        return modint(*this) -= x;
+    }
+    constexpr modint operator * (const modint& x) const {
+        return modint(*this) *= x;
+    }
+    constexpr modint operator / (const modint& x) const {
+        return modint(*this) /= x;
+    }
+    constexpr modint& operator += (const modint& x) {
+        if((val += x.val) >= MOD) val -= MOD;
+        return *this;
+    }
+    constexpr modint& operator -= (const modint& x) {
+        if((val += MOD - x.val) >= MOD) val -= MOD;
+        return *this;
+    }
+    constexpr modint& operator *= (const modint& x) {
+        (val *= x.val) %= MOD;
+        return *this;
+    }
+    constexpr modint& operator /= (const modint& x){
+        return (*this) *= x.inv();
+    }
+    constexpr bool operator == (const modint& x) const {
+        return this->val == x.val;
+    }
+    constexpr bool operator != (const modint& x) const {
+        return this->val != x.val;
+    }
+    constexpr modint pow(ll t) const {
+        if(t == 0) return 1;
+        modint x = *this, res = 1;
+        while(t > 0){
+            if(t & 1) res *= x;
+            x *= x;
+            t >>= 1;
+        }
+        return res;
+    }
+    constexpr modint inv() const {
+        ll a = val, b = MOD, u = 1, v = 0;
+        while(b > 0){
+            ll t = a / b;
+            swap(a -= t * b, b);
+            swap(u -= t * v, v);
+        }
+        return modint(u);
+    }
+    friend constexpr istream& operator >> (istream& is, modint& x) {
+        is >> x.val;
+        x.val %= MOD;
+        if(x.val < 0) x.val += MOD;
+        return is;
+    }
+    friend constexpr ostream& operator << (ostream& os, const modint& x) {
+        return os << x.val;
+    }
+};
+
+const int MOD = 998244353;
+using mint = modint<MOD>;
+
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(20);
+    int N;
+    string S;
+    cin >> N >> S;
+    // 1 以外の数字が連続していたら, 数列の大きさが 1 になることはない
+    for(int i = 0; i < N - 1; i++){
+        if(S[i] != '1' && S[i + 1] != '1'){
+            cout << -1 << endl;
+            return 0;
+        }
+    }
+
+    mint ans = 0;
+    for(int i = N - 1; i >= 1; i--){
+        ans++; // S[i] 番目の文字を消すのに必要なコスト
+        ans += ans * (S[i] - '1'); // S[i] が今までに増やした 1 の個数
+    }
+
+    cout << ans << endl;
+}
