c1168d1c529301f6df08059dccf1fdab76ca7201
Jikky <Jikky0110@gmail.com>
2023-08-15 22:16:38 +0900

    Update: c.cpp

--- ABC/ABC200-249/ABC218/c.cpp
+++ ABC/ABC200-249/ABC218/c.cpp
@@ -2,74 +2,70 @@
 using namespace std;
 using ll = long long;
 
-// 90度回転
-vector<string> rot(vector<string> grid){
-    int n = grid.size();
-    vector<string> res(n, string(n,'.'));
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            res[i][j] = grid[n - 1 - j][i];
+#ifdef LOCAL
+#include <debug_print.hpp>
+#define debug(...) debug_print::multi_print(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...) (static_cast<void>(0))
+#endif
+
+// H 行 W 列の V を 90 度回転させたものを返す
+template <class T>
+vector<vector<T>> rotate(const vector<vector<T>>& V) {
+    int H = V.size(), W = V.front().size();
+    vector res(W, vector<T>(H));
+    for (int i = 0; i < H; i++) {
+        for (int j = 0; j < W; j++) {
+            res[W - 1 - j][i] = V[i][j];
         }
     }
     return res;
 }
 
-void left_top(vector<string> &grid,int &x,int &y){
-    int n = grid.size();
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            if(grid[i][j] == '#'){
-                x = min(x,i), y = min(y,j);
-            }
+// H 行 W 列の V を bg 以外を全て含む最小矩形でトリミングしたものを返す
+template <class T>
+vector<vector<T>> trim(const vector<vector<T>>& V, char bg = '.'){
+    int H = V.size(), W = V.front().size();
+    int minH = H, minW = W, maxH = -1, maxW = -1;
+    for(int i = 0; i < H; i++){
+        for(int j = 0; j < W; j++){
+            if(V[i][j] == bg) continue;
+            minH = min(minH, i);
+            maxH = max(maxH, i);
+            minW = min(minW, j);
+            maxW = max(maxW, j);
         }
     }
+
+    // bg 以外の要素がなければ空の配列を返す
+    if(minH == H) return vector<vector<T>>();
+    vector res(maxH - minH + 1, vector<T>(maxW - minW + 1));
+    for(int i = 0; i < maxH - minH + 1; i++){
+        for(int j = 0; j < maxW - minW + 1; j++) res[i][j] = V[i + minH][j + minW];
+    }
+    return res;
 }
 
-int main(){
+int main() {
     cin.tie(nullptr);
     ios::sync_with_stdio(false);
     cout << fixed << setprecision(20);
-    int n;
-    cin >> n;
-    vector<string> s(n),t(n);
-    for(int i = 0; i < n; i++) cin >> s[i];
-    for(int i = 0; i < n; i++) cin >> t[i];
-
-
-    // #の数が異なればNo
-    int cnt_s = 0, cnt_t = 0;
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            if(s[i][j] == '#') cnt_s++;
-            if(t[i][j] == '#') cnt_t++;
-        }
+    int N;
+    cin >> N;
+    vector S(N, vector<char>(N)), T(N, vector<char>(N));
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) cin >> S[i][j];
     }
-    if(cnt_s != cnt_t){
-        cout << "No" << endl;
-        return 0;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) cin >> T[i][j];
     }
 
-    // 4回判定
+    S = trim(S), T = trim(T);
+    bool same = false;
     for(int i = 0; i < 4; i++){
-        int sx = n, sy = n, tx = n, ty = n;
-        left_top(s, sx, sy);
-        left_top(t, tx, ty);
-
-        bool flag = true;
-        for(int x = 0; x < n; x++){
-            for(int y = 0; y < n; y++){
-                if( n <= sx+x || n <= sy+y || n <= tx+x || n <= ty+y) continue;
-                if(s[sx+x][sy+y] != t[tx+x][ty+y]) flag = false;
-            }
-        }
-
-        if(flag){
-            cout << "Yes" << endl;
-            return 0; 
-        }
-        s = rot(s);
+        S = rotate(S);
+        if(S == T) same = true;
     }
 
-    cout << "No" << endl;
-    return 0;
+    cout << (same ? "Yes" : "No") << endl;
 }
