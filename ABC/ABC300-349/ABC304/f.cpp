a0757a02f22d4b6e5f10001e64cd1386eb8a2d5c
Jikky <Jikky0110@gmail.com>
2024-02-14 23:46:58 +0900

    Add: f.cpp

--- ABC/ABC300-349/ABC304/f.cpp
+++ ABC/ABC300-349/ABC304/f.cpp
@@ -9,129 +9,136 @@ using ll = long long;
 #define debug(...) (static_cast<void>(0))
 #endif
 
-// xの約数を列挙した配列を返す
-vector<ll> make_is_divisors(ll x){
-    vector<ll> divisors;
-    for(ll i = 1; i * i <= x; i++){
-        if(x % i == 0){
-            divisors.push_back(i);
-            if(x / i != i) divisors.push_back(x / i);
-        }
-    }
-    sort(divisors.begin(), divisors.end());
-    return divisors;
-}
-
-template<int MOD> struct Mint {
+template<int MOD> struct modint {
     ll val;
-    constexpr Mint(ll x = 0): val(x % MOD) {
+    constexpr modint(ll x = 0): val(x % MOD) {
         if(val < 0) val += MOD;
     }
-    constexpr int getmod() const { return MOD; }
-    constexpr Mint operator + () const {
-        return Mint(val);
+    constexpr int mod() const { return MOD; }
+    constexpr modint operator + () const {
+        return modint(val);
     }
-    constexpr Mint operator - () const {
-        return Mint(MOD - val);
+    constexpr modint operator - () const {
+        return modint(MOD - val);
     }
-    constexpr Mint operator + (const Mint& x) const {
-        return Mint(*this) += x;
+    constexpr modint& operator++() {
+        return (*this) += 1;
     }
-    constexpr Mint operator - (const Mint& x) const {
-        return Mint(*this) -= x;
+    constexpr modint& operator--() {
+        return (*this) -= 1;
     }
-    constexpr Mint operator * (const Mint& x) const {
-        return Mint(*this) *= x;
+    constexpr modint operator++(int) {
+        modint x = (*this); ++(*this);
+        return x;
     }
-    constexpr Mint operator / (const Mint& x) const {
-        return Mint(*this) /= x;
+    constexpr modint operator--(int) {
+        modint x = (*this); --(*this);
+        return x;
     }
-    constexpr Mint& operator += (const Mint& x) {
+    constexpr modint operator + (const modint& x) const {
+        return modint(*this) += x;
+    }
+    constexpr modint operator - (const modint& x) const {
+        return modint(*this) -= x;
+    }
+    constexpr modint operator * (const modint& x) const {
+        return modint(*this) *= x;
+    }
+    constexpr modint operator / (const modint& x) const {
+        return modint(*this) /= x;
+    }
+    constexpr modint& operator += (const modint& x) {
         if((val += x.val) >= MOD) val -= MOD;
         return *this;
     }
-    constexpr Mint& operator -= (const Mint& x) {
+    constexpr modint& operator -= (const modint& x) {
         if((val += MOD - x.val) >= MOD) val -= MOD;
         return *this;
     }
-    constexpr Mint& operator *= (const Mint& x) {
+    constexpr modint& operator *= (const modint& x) {
         (val *= x.val) %= MOD;
         return *this;
     }
-    constexpr Mint& operator /= (const Mint& x){
+    constexpr modint& operator /= (const modint& x){
         return (*this) *= x.inv();
     }
-    constexpr bool operator == (const Mint& x) const {
+    constexpr bool operator == (const modint& x) const {
         return this->val == x.val;
     }
-    constexpr bool operator != (const Mint& x) const {
+    constexpr bool operator != (const modint& x) const {
         return this->val != x.val;
     }
-    constexpr Mint pow(ll t) const {
+    constexpr modint pow(ll t) const {
         if(t == 0) return 1;
-        Mint x = pow(t >> 1);
-        x *= x;
-        if(t & 1) x *= *this;
-        return x;
+        modint x = *this, res = 1;
+        while(t > 0){
+            if(t & 1) res *= x;
+            x *= x;
+            t >>= 1;
+        }
+        return res;
     }
-    constexpr Mint inv() const {
+    constexpr modint inv() const {
         ll a = val, b = MOD, u = 1, v = 0;
         while(b > 0){
             ll t = a / b;
             swap(a -= t * b, b);
             swap(u -= t * v, v);
         }
-        return Mint(u);
+        return modint(u);
     }
-    friend constexpr istream& operator >> (istream& is, Mint& x) {
+    friend constexpr istream& operator >> (istream& is, modint& x) {
         is >> x.val;
         x.val %= MOD;
         if(x.val < 0) x.val += MOD;
         return is;
     }
-    friend constexpr ostream& operator << (ostream& os, const Mint& x) {
+    friend constexpr ostream& operator << (ostream& os, const modint& x) {
         return os << x.val;
     }
 };
 
 const int MOD = 998244353;
-using mint = Mint<MOD>;
+using mint = modint<MOD>;
 
-int main(){
+// xの約数を列挙した配列を返す
+vector<ll> make_is_divisors(ll N, bool sorted = true) {
+    vector<ll> divisors;
+    for (ll i = 1; i * i <= N; i++) {
+        if (N % i == 0) {
+            divisors.emplace_back(i);
+            if (N / i != i) divisors.emplace_back(N / i);
+        }
+    }
+    if (sorted) sort(divisors.begin(), divisors.end());
+    return divisors;
+}
+
+int main() {
     cin.tie(nullptr);
     ios::sync_with_stdio(false);
     cout << fixed << setprecision(20);
     int N;
-    cin >> N;
     string S;
-    cin >> S;
+    cin >> N >> S;
 
-    auto div = make_is_divisors(N);
-
-    vector<mint> cnt(N);
-    for(auto d: div){
-        if(d == N) continue;
-
-        // 周期 d 日のとき
-        // 周期の 1 ~ d 日目を探索
-        cnt[d] = 1;
-        for(int i = 0; i < d; i++){
-            bool flag = true;
-            // 周期中に出勤しないといけない日があるならその日は出勤しないといけない
-            for(int j = i; j < N; j += d){
-                if(S[j] == '.') flag = false;
-            }
-            if(flag) cnt[d] *= 2;
+    vector<mint> dp(N + 1);
+    for (auto&& M : make_is_divisors(N)) {
+        if(M == N) continue;
+        // 周期 M で各 i 日について全探索
+        dp[M] = 1;
+        for(int i = 0; i < M; i++){
+            // flag := kM + i 日目に高橋くんが出勤しない日があるか
+            bool flag = false;
+            for(int j = i; j < N; j += M) if(S[j] == '.') flag = true;
+            // 出勤しない日がある場合 1 通り, 出勤しない日がない場合 2 通り 
+            dp[M] *= flag ? 1 : 2;
         }
-
-        // d2 が d の約数ならその分は重複しているので引く
-        for(int d2 = 1; d2 < d; d2++){
-            if(d % d2 == 0) cnt[d] -= cnt[d2];
-        }
+        // 周期 M の約数 m についての場合が重複しているので引く
+        for(auto&& m: make_is_divisors(M)) if(m != M) dp[M] -= dp[m];
     }
 
-    mint ans = 0;
-    for(int i = 0; i < N; i++) ans += cnt[i];
-
-    cout << ans << endl;
+    debug(dp);
+    mint ans = reduce(dp.begin(), dp.end());
+    cout << ans << '\n';
 }
